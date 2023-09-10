eab06c38b1f3b390a6bd4a640b012bf6d2558aec
Whisper Snowie <104696380+Whisper-Snowie@users.noreply.github.com>
2023-09-10 13:11:57 +0200

    feat: added text to the website added the text and the icons/emoijis to the website

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
+function App () {
+    return (
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+				<div className="h-[100vh] py-[120px] px-[100px] flex items-start flex-col">
+					<h1 className="font-light text-[2.5rem] flex items-center">
+						Hello!
+						<span style={{backgroundImage: 'url("src/assets/wave.png")'}} className="cursor-pointer h-[40px] w-[40px] bg-no-repeat bg-contain inline-block text-center ml-6" />
+					</h1>
+					<div>
+					<h2 className="font-light text-[1.75rem] mt-10">
+						We`re 
+						<span className="font-bold"> Ferbion</span>
+						, a software company with the goal to <br/> unlock innovation through code.
+					</h2>
+					<h3 className="font-normal text-[1.25rem] flex items-center mt-6">
+						<span>Get in touch with us</span>
+						<span style={{backgroundImage: 'url("src/assets/pointright.png")'}} className="cursor-poiner h-[40px] w-[40px] bg-no-repeat bg-contain inline-block text-center ml-3" />
+						<span className="ml-4">
+							<a className="test border-b-2 border-[#007bff]" href="mailto:" target="_blank">
+								ferbionIT@gmail.com
+							</a>
+						</span>
+					</h3>
+				</div>
+				</div>
+			</div>
+    )
 }
 
 export default App
