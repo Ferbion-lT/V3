72192b2f7a0913e7ec885729e29551cdb02324df
Whisper Snowie <104696380+Whisper-Snowie@users.noreply.github.com>
2023-11-11 01:55:03 +0100

    feat: Added the navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="bg-[#272d30]">
+    <div class="">
+      <ul class="flex flex-row gap-10">
+        <li class="list-none">
+          <a class="text-white uppercase">home</a>
+        </li>
+        <li class="list-none">
+          <a class="text-white">about us</a>
+        </li>
+        <li class="list-none ">
+          <a class="text-white">services</a>
+        </li>
+      </ul>
+    </div>
+    <div>
+      <img src="../assets/Logo.png"> 
+    </div>
+    <div class="">
+      <ul class="flex flex-row gap-10">
+        <li class="list-none">
+          <a class="text-white uppercase">projects</a>
+        </li>
+        <li class="list-none">
+          <a class="text-white uppercase">contact</a>
+        </li>
+        <li class="list-none">
+          <a class="text-white uppercase">impressum</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
