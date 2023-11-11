a5ced6f09afb7837d7ee602d4273a2c7acb391ae
linusehle <linusfinn.ehle@gmail.com>
2023-11-11 15:36:05 +0100

    feat: Inital commit

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,17 @@
       <div class="text-[#58a6ff] uppercase font-bold text-6xl text-center pt-5">
         <h2>Ferbion</h2>
       </div>
+      <div class="text-white uppercase font-bold text-5xl pt-[250px] pl-8">
+        <h2>We make Websites for you</h2>
+      </div>
+      <div class="flex">
+        <div class="uppercase">
+          <button class="bg-[#41bcff]">Hire us</button>
+        </div>
+        <div class="uppercase">
+          <button class="bg-[#545454] border-white border-2">Learn more</button>
+        </div>
+      </div>
     </div>
     <div class="bg-white w-full h-screen"></div>
   </div>
