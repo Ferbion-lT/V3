d9372ae2c32ba71498eacd93754d4bd5555dd205
linusehle <linusfinn.ehle@gmail.com>
2023-11-16 09:32:35 +0100

    feat: added the sideshow carousel

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,5 +1,112 @@
 <template>
-    <div>
-        Hallo
-    </div>
+<div class="wrapper">
+	<div class="slider">
+		<div class="slide-track">
+			<div class="slide">
+				<img src="img/Spiderman.png" />
+			</div>
+			
+			<div class="slide">
+				<img src="img/Spiderman.png" />
+			</div>
+			<div class="slide">
+				<img src="img/Black-Adam.png" />
+			</div>
+			<div class="slide">
+				<img src="img/Iron-Man.png" />
+			</div>
+			<div class="slide">
+				<img src="img/SuperMan.png" />
+			</div>
+			<div class="slide">
+				<img src="img/Captain-America.png" />
+			</div>
+			<div class="slide">
+				<img src="img/Black-Panther.png" />
+			</div>
+			<div class="slide">
+				<img src="img/Aquaman.png" />
+			</div>
+			<div class="slide">
+				<div class="bg-[#272d30] h-[150px] w-[150px] flex justify-center items-center flex-col rounded-[15px]">
+					<span class="text-[#01a7ee] text-3xl font-bold">+80</span><br>
+					<span class="text-white">Websites</span>
+				</div>
+			</div>
+			<div class="slide">
+				<img src="img/Flash.png" />
+			</div>
+			<div class="slide">
+				<div class="bg-[#272d30] h-[150px] w-[150px] flex justify-center items-center flex-col rounded-[15px]">
+					<span class="text-[#01a7ee] text-3xl font-bold">+60</span><br>
+					<span class="text-white">5 Stars Ratings</span>
+				</div>
+			</div>
+			<div class="slide">
+				<img src="img/Ant-Man.png" />
+			</div>
+			<div class="slide">
+				<img src="img/Wonder-Woman.png" />
+			</div>			
+			<div class="slide">
+				<img src="img/Green-Lantern.png" />
+			</div>
+		</div>
+	</div>
+</div>
 </template>
+
+<style>
+
+.wrapper {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	height: 95vh;
+}
+
+.slider {
+	height: 200px;
+	margin: auto;
+	overflow: hidden;
+	position: relative;
+	width: 80%;
+}
+
+.slider .slide-track {
+	-webkit-animation: scroll 30s linear infinite;
+	animation: scroll 30s linear infinite;
+	display: flex;
+	width: calc(250px * 14);
+}
+
+.slider .slide {
+	height: 100px;
+	width: 250px;
+}
+
+.slider .slide img {
+    width: 80%;
+	height: 100px;
+	border-radius: 10px;
+	border: 2px solid #bbbbbb;
+}
+
+@-webkit-keyframes scroll {
+	0% {
+		transform: translateX(0);
+	}
+	100% {
+		transform: translateX(calc(-250px * 7));
+	}
+}
+
+@keyframes scroll {
+	0% {
+		transform: translateX(0);
+	}
+	100% {
+		transform: translateX(calc(-250px * 7));
+	}
+}
+</style>
