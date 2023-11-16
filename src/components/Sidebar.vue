e7d5b3a2c3f98afcb990dbfaccd797c42aa92d27
linusehle <linusfinn.ehle@gmail.com>
2023-11-16 13:03:36 +0100

    feat: converted Css to TailwindCss

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,69 +1,69 @@
 <template>
-<div class="wrapper">
-	<div class="slider">
-		<div class="slide-track">
-			<div class="slide">
+<div class="flex justify-center">
+	<div class="flex text-white bg-[#0f0f0f] w-full relative overflow-hidden pt-[25px] m-auto h-[200px]">
+		<div class="slide-animation flex">
+			<div class="pl-[25px]">
 				<div class="bg-[#272d30] h-[150px] w-[150px] flex justify-center items-center flex-col rounded-[15px]">
 					<span class="text-[#01a7ee] text-3xl font-bold">+80</span><br>
 					<span class="text-white">Websites</span>
 				</div>
 			</div>
-			<div class="slide">
+			<div class="pl-[25px]">
 				<div class="h-[175px] w-[450px] flex items-center">
 					<span class="text-2xl">-- We've Made Already +80 Websites --</span>
 				</div>
 			</div>
-			<div class="slide">
+			<div class="pl-[25px]">
 				<div class="bg-[#272d30] h-[150px] w-[150px] flex justify-center items-center flex-col rounded-[15px]">
 					<span class="text-[#01a7ee] text-3xl font-bold">+60</span><br>
 					<span class="text-white">5 Stars Ratings</span>
 				</div>
 			</div>
-			<div class="slide">
+			<div class="pl-[25px]">
 				<div class="h-[175px] w-[300px] flex items-center">
 					<span class="text-2xl">-- 5 Stars Ratings, Easy! --</span>
 				</div>
 			</div>
-			<div class="slide">
+			<div class="pl-[25px]">
 				<div class="bg-[#272d30] h-[150px] w-[150px] flex justify-center items-center flex-col rounded-[15px]">
 					<span class="text-[#01a7ee] text-3xl font-bold">+580</span><br>
 					<span class="text-white">Hours Of Work</span>
 				</div>
 			</div>			
-			<div class="slide">
+			<div class="pl-[25px]">
 				<div class="h-[175px] w-[475px] flex items-center">
 					<span class="text-2xl">-- We Do Our Best For Happy Customers --</span>
 				</div>
 			</div>
-			<div class="slide">
+			<div class="pl-[25px]">
 				<div class="bg-[#272d30] h-[150px] w-[150px] flex justify-center items-center flex-col rounded-[15px]">
 					<span class="text-[#01a7ee] text-3xl font-bold">+80</span><br>
 					<span class="text-white">Websites</span>
 				</div>
 			</div>
-			<div class="slide">
+			<div class="pl-[25px]">
 				<div class="h-[175px] w-[450px] flex items-center">
 					<span class="text-2xl">-- We've Made Already +80 Websites --</span>
 				</div>
 			</div>
-			<div class="slide">
+			<div class="pl-[25px]">
 				<div class="bg-[#272d30] h-[150px] w-[150px] flex justify-center items-center flex-col rounded-[15px]">
 					<span class="text-[#01a7ee] text-3xl font-bold">+60</span><br>
 					<span class="text-white">5 Stars Ratings</span>
 				</div>
 			</div>
-			<div class="slide">
+			<div class="pl-[25px]">
 				<div class="h-[175px] w-[300px] flex items-center">
 					<span class="text-2xl">-- 5 Stars Ratings, Easy! --</span>
 				</div>
 			</div>
-			<div class="slide">
+			<div class="pl-[25px]">
 				<div class="bg-[#272d30] h-[150px] w-[150px] flex justify-center items-center flex-col rounded-[15px]">
 					<span class="text-[#01a7ee] text-3xl font-bold">+580</span><br>
 					<span class="text-white">Hours Of Work</span>
 				</div>
 			</div>			
-			<div class="slide">
+			<div class="pl-[25px]">
 				<div class="h-[175px] w-[475px] flex items-center">
 					<span class="text-2xl">-- We Do Our Best For Happy Customers --</span>
 				</div>
@@ -73,35 +73,10 @@
 </div>
 </template>
 <style>
-.wrapper {
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	height: 80vh;
-}
 
-.slider {
-	height: 200px;
-	padding-top: 25px;
-	margin: auto;
-	overflow: hidden;
-	position: relative;
-	width: 100%;
-	display: flex;
-	background-color: #0f0f0f;
-	color: white;
-}
-
-.slider .slide-track {
+.slide-animation {
 	-webkit-animation: scroll 30s linear infinite;
 	animation: scroll 30s linear infinite;
-	display: flex;
-	width: calc(250px * 6);
-}
-
-.slider .slide {
-	height: 100px;
-	padding-left: 25px;
 }
 
 @-webkit-keyframes scroll {
