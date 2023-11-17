1becda426373a389f8d904bd1a69a9cf71e88f24
linusehle <linusfinn.ehle@gmail.com>
2023-11-17 23:52:39 +0100

    feat: Imported Footer.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup>
  import Navbar from './components/Navbar.vue'
- import Sidebar from './components/Sidebar.vue'
+ import Sidebar from './components/Slideshow.vue'
+ import About from './components/About.vue'
+ import Footer from './components/Footer.vue'
 </script>
 
 <template>
     <Navbar />
     <Sidebar />
+    <About />
+    <Footer />
 </template>
