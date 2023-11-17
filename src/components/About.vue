9ed47a5c3ca0bf7b2fc96bf985ab500ef79c6564
linusehle <linusfinn.ehle@gmail.com>
2023-11-17 23:38:44 +0100

    feat: Inital commit

---
+++ src/components/About.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="bg-[#272d30] h-screen">
+            <div class="pt-[50px] pl-[100px]">
+                <span class="text-[#01a7ee] uppercase font-bold text-xl">about us</span>
+            </div>
+        </div>
+</template>
