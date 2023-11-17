4655bd4cf628b977f00204cc7e023b07b7368dab
linusehle <linusfinn.ehle@gmail.com>
2023-11-17 23:53:01 +0100

    feat: added the footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,64 @@
 <template>
-    <div class="bg-[#191d1e]">
-        <div class="flex flex-row">
-            <div>Hallo</div>
+    <div class="bg-[#191d1e] flex justify-center">
+        <div class="flex flex-col border-t-2 border-[#01a7ee]">
+            <div class="pt-[75px] text-center">
+                <span class="uppercase text-[#515151] font-bold text-5xl">follow us!</span>
+            </div>
+            <div class="flex flex-row gap-5 pt-[25px]">
+                <div class="h-[50px] w-[50px] rounded-md bg-[#515151]">
+
+                </div>
+                <div class="h-[50px] w-[50px] rounded-md bg-[#515151]">
+
+                </div>
+                <div class="h-[50px] w-[50px] rounded-md bg-[#515151]">
+
+                </div>
+                <div class="h-[50px] w-[50px] rounded-md bg-[#515151]">
+
+                </div>
+                <div class="h-[50px] w-[50px] rounded-md bg-[#515151]">
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="bg-[#191d1e] flex justify-center gap-[250px]">
+        <div class="flex flex-col">
+            <div class="border-l-2 border-[#01a7ee]">
+                <ul>
+                    <li class="list-none">
+                        <a class="text-[#515151]">Impressum</a>
+                    </li>
+                    <li class="list-none">
+                        <a class="text-[#515151]">Datenschutz</a>
+                    </li>
+                    <li class="list-none">
+                        <a class="text-[#515151]">Privatsphäre</a>
+                    </li>
+                    <li class="list-none">
+                        <a class="text-[#515151]">Cookies</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="flex flex-col">
+            <div class="border-r-2 border-[#01a7ee]">
+                <ul>
+                    <li class="list-none">
+                        <a class="text-[#515151]">Home</a>
+                    </li>
+                    <li class="list-none">
+                        <a class="text-[#515151]">About us</a>
+                    </li>
+                    <li class="list-none">
+                        <a class="text-[#515151]">Projects</a>
+                    </li>
+                    <li class="list-none">
+                        <a class="text-[#515151] underline">Contact</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 </template>
