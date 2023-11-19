b6d0042fd137f05465d7e214f381351ca0723fb9
linusehle <linusfinn.ehle@gmail.com>
2023-11-19 16:58:28 +0100

    feat: added the profile card

--- src/components/About.vue
+++ src/components/About.vue
@@ -3,5 +3,71 @@
             <div class="pt-[50px] pl-[100px]">
                 <span class="text-[#01a7ee] uppercase font-bold text-xl">about us</span>
             </div>
+            <div class="flex justify-start pl-[100px] pt-[75px]">
+                <div class="flex flex-col text-center">
+                    <span class="text-white font-bold text-4xl text-left">A team with a true<br><span class="text-[#01a7ee]">friendship </span>behind it</span>
+                    <span class="text-[#adadad] mt-[25px] text-left">We have both been trapped in the depths of<br>
+                    web development and web design since 2019.<br>
+                    If you want to know more, just contact us! ;)
+                    </span>
+                    <div>
+                        <button class="uppercase bg-[#e69700] rounded-[15px] h-[50px] w-[200px] font-bold text-2xl text-[#272d30] mt-[75px]">contact</button>
+                    </div>
+                </div>
+            </div>
+            <div class="flex flex-row">
+                <div class="bg-[#0f0f0f] text-white rounded-[15px]">
+                    <div>
+                        <div>
+
+                        </div>
+                        <div class="flex flex-col">
+                            <span>Linus Ehle</span>
+                            <div class="flex">
+                                <img>
+                                <span>Stuttgart, Germany</span>
+                            </div>
+                        </div>
+                        <div class="flex flex-row">
+                            <div class="flex flex-col">
+                                <span>Experience</span>
+                                <span>4 Years</span>
+                            </div>
+                            <div class="flex flex-col">
+                                <span>Projects</span>
+                                <span>2 Projects</span>
+                            </div>
+                        </div>
+                        <div>
+                            <span>Hi, I am Linus Ehle. I started programming when I was 11.<br> 
+                            My first programming language was Python and I learned more<br>
+                            and more by looking at other programming languages and<br>
+                            coding with them. Among the programming languages<br>
+                            I know by now are: Python, (Html,Css),<br>
+                            Javascript, C/C++/C#, Php, Java, Typescript, Kotlin and Rust.<br>
+                            As well as some frameworks like VueJs. </span>
+                        </div>
+                        <div>
+                            <div>
+                                <span>Skill</span>
+                            </div>
+                            <div class="flex flex-row gap-3">
+                                <div class="bg-[#]">
+                                    <span>Html & Css</span>
+                                </div>
+                                <div class="bg-[#]">
+                                    <span>Javascript</span>
+                                </div>
+                                <div class="bg-[#]">
+                                    <span>Vue JS</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+
+                </div>
+            </div>
         </div>
 </template>
