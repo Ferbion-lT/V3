f84042b0f21f00ad925587dc1367024694c6ae25
linusehle <linusfinn.ehle@gmail.com>
2023-11-23 19:25:35 +0100

    feat: Inital commit

---
+++ src/components/Services.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="bg-[#191d1e]">
+        <div class="">
+
+        </div>
+        <div class="flex flex-row">
+            <div class="flex flex-col">
+                <div class="bg-[#272d30]">
+                    <div>
+                        <img class="" src="../assets/create.png" />
+                    </div>
+                    <div>
+                        <span class="text-[#01a7ee] font-bold">Create</span>
+                    </div>
+                    <div>
+                        <span>Are you expanding your business or<br>
+                        do you need a website as an artist?<br>
+                        Then you've come to the right place.
+                        </span>
+                    </div>
+                </div>
+                <div class="bg-[]">
+                    <span class="font-bold uppercase">create</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
