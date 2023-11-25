d4d8a9f3000449b42f48e1e2c30d8422152b1781
linusehle <linusfinn.ehle@gmail.com>
2023-11-25 22:06:19 +0100

    feat: adde the business section and the choosing section

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,119 +1,66 @@
 <template>
-    <div class="bg-[#272d30] h-screen">
-            <div class="pt-[50px] pl-[100px]">
-                <span class="text-[#01a7ee] uppercase font-bold text-xl">about us</span>
-            </div>
-            <div class="flex justify-start pl-[100px] pt-[75px]">
-                <div class="flex flex-col text-center">
-                    <span class="text-white font-bold text-4xl text-left">A team with a true<br><span class="text-[#01a7ee]">friendship </span>behind it</span>
-                    <span class="text-[#adadad] mt-[25px] text-left">We have both been trapped in the depths of<br>
+    <div class="bg-[#272d30] pb-[100px]">   
+        <div class="flex flex-row gap-[500px]">
+            <div class="flex flex-col">
+                <div class="pt-[50px] pl-[100px]">
+                    <span class="text-[#01a7ee] uppercase font-bold text-xl">about us</span>
+                </div>
+                <div class="pt-[50px] pl-[100px]">
+                    <span class="text-white font-bold text-4xl">A team with a true<br><span class="text-[#01a7ee]">friendship </span>behind it</span>
+                </div>
+                <div class="pt-[50px] pl-[100px]">
+                    <span class="text-white text-xl">We have both been trapped in the depths of<br>
                     web development and web design since 2019.<br>
                     If you want to know more, just contact us! ;)
-                    </span>
-                    <div>
-                        <button class="uppercase bg-[#e69700] rounded-[15px] h-[50px] w-[200px] font-bold text-2xl text-[#272d30] mt-[75px]">contact</button>
-                    </div>
-                </div>
+                </span>
             </div>
-            <div class="flex flex-row gap-6">
-                <div class="bg-[#0f0f0f] text-white rounded-[15px]">
-                    <div>
-                        <div>
-
-                        </div>
-                        <div class="flex flex-col pl-[15px]">
-                            <span class="text-xl">Linus Ehle</span>
-                            <div class="flex">
-                                <img>
-                                <span class="text-base">Stuttgart, Germany</span>
-                            </div>
-                        </div>
-                        <div class="flex flex-row pl-[15px]">
-                            <div class="flex flex-col">
-                                <span>Experience</span>
-                                <span>4 Years</span>
-                            </div>
-                            <div class="flex flex-col">
-                                <span>Projects</span>
-                                <span>2 Projects</span>
-                            </div>
-                        </div>
-                        <div class="pl-[15px]">
-                            <span>Hi, I am Linus Ehle. I started programming when I was 11.<br> 
-                            My first programming language was Python and I learned more<br>
-                            and more by looking at other programming languages and<br>
-                            coding with them. Among the programming languages<br>
-                            I know by now are: Python, (Html,Css),<br>
-                            Javascript, C/C++/C#, Php, Java, Typescript, Kotlin and Rust.<br>
-                            As well as some frameworks like VueJs. </span>
-                        </div>
-                        <div class="pl-[15px]">
-                            <div>
-                                <span>Skill</span>
-                            </div>
-                            <div class="flex flex-row gap-3">
-                                <div class="bg-[#272d30] h-[40px] w-[125px] flex items-center justify-center rounded-[7.5px]">
-                                    <span>Html & Css</span>
-                                </div>
-                                <div class="bg-[#272d30] h-[40px] w-[125px] flex items-center justify-center rounded-[7.5px]">
-                                    <span>Javascript</span>
-                                </div>
-                                <div class="bg-[#272d30] h-[40px] w-[125px] flex items-center justify-center rounded-[7.5px]">
-                                    <span>Vue JS</span>
-                                </div>
-                            </div>
-                        </div>
+            <div class="pt-[75px] pl-[200px]">
+                <div class="bg-[#e69700] rounded-[15px] text-center w-[175px] h-[40px] flex justify-center items-center uppercase text-black font-bold text-3xl">contact</div>
+            </div>
+            </div>
+            <div class="flex flex-row gap-10 pt-[75px]">
+                <div class="w-[300px] rounded-[15px]">
+                    <div class="bg-white h-[300px] rounded-t-[15px]"></div>
+                    <div class="bg-[#2e2e2e] rounded-b-[15px] h-[50px] text-center flex items-center justify-center">
+                        <span class="text-[#01a7ee] font-bold text-3xl">Linus</span>
+                    </div>
+                    <div class="ml-[10px] mt-4 text-center border-l-4 border-l-[#01a7ee]">
+                        <span class="text-white">Since 2017 Html, Css and Javascript</span><br>
+                        <span class="text-white text-left">Since 2018 Vue and React</span>
                     </div>
                 </div>
-                <div class="bg-[#0f0f0f] text-white rounded-[15px]">
-                    <div>
-                        <div>
-
-                        </div>
-                        <div class="flex flex-col pl-[15px]">
-                            <span class="text-xl">Ben Luca Gering</span>
-                            <div class="flex">
-                                <img>
-                                <span class="text-base">Colegne, Germany</span>
-                            </div>
-                        </div>
-                        <div class="flex flex-row pl-[15px]">
-                            <div class="flex flex-col">
-                                <span>Experience</span>
-                                <span>4 Years</span>
-                            </div>
-                            <div class="flex flex-col">
-                                <span>Projects</span>
-                                <span>2 Projects</span>
-                            </div>
-                        </div>
-                        <div class="pl-[15px]">
-                            <span>Hi, I am Linus Ehle. I started programming when I was 11.<br> 
-                            My first programming language was Python and I learned more<br>
-                            and more by looking at other programming languages and<br>
-                            coding with them. Among the programming languages<br>
-                            I know by now are: Python, (Html,Css),<br>
-                            Javascript, C/C++/C#, Php, Java, Typescript, Kotlin and Rust.<br>
-                            As well as some frameworks like VueJs. </span>
-                        </div>
-                        <div class="pl-[15px]">
-                            <div>
-                                <span>Skill</span>
-                            </div>
-                            <div class="flex flex-row gap-3">
-                                <div class="bg-[#272d30] h-[40px] w-[125px] flex items-center justify-center rounded-[7.5px]">
-                                    <span>Html & Css</span>
-                                </div>
-                                <div class="bg-[#272d30] h-[40px] w-[125px] flex items-center justify-center rounded-[7.5px]">
-                                    <span>Javascript</span>
-                                </div>
-                                <div class="bg-[#272d30] h-[40px] w-[125px] flex items-center justify-center rounded-[7.5px]">
-                                    <span>Vue JS</span>
-                                </div>
-                            </div>
-                        </div>
+                <div class="w-[300px] rounded-[15px]">
+                    <div class="bg-white h-[300px] rounded-t-[15px]"></div>
+                    <div class="bg-[#2e2e2e] rounded-b-[15px] h-[50px] text-center flex items-center justify-center">
+                        <span class="text-[#01a7ee] font-bold text-3xl">Ben-Luca</span>
+                    </div>
+                    <div class="ml-[10px] mt-4 text-center border-l-4 border-l-[#01a7ee]">
+                        <span class="text-white">Since 2017 Html, Css and Javascript</span><br>
+                        <span class="text-white text-left">Since 2018 Vue and React</span>
                     </div>
                 </div>
             </div>
         </div>
+        <div class="mt-[250px] flex flex-row justify-center gap-[250px]">
+            <div class="bg-white h-[175px] w-[400px] rounded-[15px]"></div>
+            <div class="flex flex-col gap-8">
+                <span class="text-white font-bold text-4xl text-right">Take your business to<br>the <span class="text-[#01a7ee] font-bold">next level</span></span>
+                <span class="text-white text-xl text-right">If you want new customers then you are right here!<br>
+                We make your website interesting and we make it modern
+                </span>
+            </div>
+        </div>
+        <div class="mt-[250px] flex flex-row justify-center gap-[250px]">
+            <div class="flex flex-col gap-12">
+                <span class="text-white font-bold text-4xl">Why You Should <span class="text-[#01a7ee]">Choose Us?</span></span>
+                <div class="flex flex-col border-l-2 border-l-[#01a7ee] pl-4 ml-[75px]">
+                    <span class="text-white text-xl">4 Years of experience on the web</span>
+                    <span class="text-white text-xl">A young team of experienced programmers</span>
+                    <span class="text-white text-xl">Reliable and friendly</span>
+                    <span class="text-white text-xl">Respectful interaction with customers</span>
+                </div>
+            </div>
+            <div class="bg-white h-[175px] w-[400px] rounded-[15px]"></div>
+        </div>
+    </div>
 </template>
