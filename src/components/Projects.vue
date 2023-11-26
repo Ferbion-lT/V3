160b24452fdb9c8c685a18b8883629e3a108a3ce
linusehle <linusfinn.ehle@gmail.com>
2023-11-26 15:55:39 +0100

    feat: made everything responsive

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,23 +1,27 @@
 <template>
     <div class="bg-[#272d30]">
-        <div class="pt-[50px] pl-[100px]">
+        <div class="pt-[50px] flex justify-center sm:pl-[100px] sm:block">
             <span class="text-[#01a7ee] uppercase font-bold text-xl">projects</span>
         </div>
-        <div class="pt-[100px flex flex-row justify-center gap-[250px] pt-[250px]">
-            <div class="flex flex-col gap-4">
+        <div class="flex flex-col gap-[250px] pt-[250px]">
+            <div class="flex justify-center flex-col gap-4">
                 <div class="text-center">
                     <span class="text-white font-bold text-4xl uppercase">name</span>
                 </div>
-                <div class="rounded-[15px] bg-white h-[150px] w-[300px]"></div>
+                <div class="flex justify-center">
+                    <div class="rounded-[15px] bg-white h-[150px] w-[300px]"></div>
+                </div>
                 <div class="flex justify-center pt-10">
                     <div class="bg-[#e69700] rounded-[15px] text-center w-[125px] h-[40px] flex justify-center items-center uppercase text-black font-bold text-2xl">visit</div>
                 </div>
             </div>
-            <div class="flex flex-col gap-4">
+            <div class="flex flex-col justify-center gap-4">
                 <div class="text-center">
                     <span class="text-white font-bold text-4xl uppercase">name</span>
                 </div>
-                <div class="rounded-[15px] bg-white h-[150px] w-[300px]"></div>
+                <div class="flex justify-center">
+                    <div class="rounded-[15px] bg-white h-[150px] w-[300px]"></div>
+                </div>
                 <div class="flex justify-center pt-10">
                     <div class="bg-[#e69700] rounded-[15px] text-center w-[125px] h-[40px] flex justify-center items-center uppercase text-black font-bold text-2xl">visit</div>
                 </div>
