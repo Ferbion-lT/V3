16a77d3957e93dabe47b3fbb76b98545aabf1679
linusehle <linusfinn.ehle@gmail.com>
2023-11-26 12:28:20 +0100

    feat: Inital commit

---
+++ src/components/Projects.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="bg-[#272d30]">
+        <div class="pt-[50px] pl-[100px]">
+            <span class="text-[#01a7ee] uppercase font-bold text-xl">projects</span>
+        </div>
+        <div class="pt-[100px flex flex-row justify-center gap-[250px] pt-[250px]">
+            <div class="flex flex-col gap-4">
+                <div class="text-center">
+                    <span class="text-white font-bold text-4xl uppercase">name</span>
+                </div>
+                <div class="rounded-[15px] bg-white h-[150px] w-[300px]"></div>
+                <div class="flex justify-center pt-10">
+                    <div class="bg-[#e69700] rounded-[15px] text-center w-[125px] h-[40px] flex justify-center items-center uppercase text-black font-bold text-2xl">visit</div>
+                </div>
+            </div>
+            <div class="flex flex-col gap-4">
+                <div class="text-center">
+                    <span class="text-white font-bold text-4xl uppercase">name</span>
+                </div>
+                <div class="rounded-[15px] bg-white h-[150px] w-[300px]"></div>
+                <div class="flex justify-center pt-10">
+                    <div class="bg-[#e69700] rounded-[15px] text-center w-[125px] h-[40px] flex justify-center items-center uppercase text-black font-bold text-2xl">visit</div>
+                </div>
+            </div>
+        </div>
+        <div class="pt-[250px] flex justify-center">
+            <span class="text-[#01a7ee] font-bold text-4xl text-center">4 Years <span class="text-white">Of Development<br>Experience</span></span>
+        </div>
+        <div class="pt-[100px flex flex-row justify-center gap-[250px] pt-[250px] pb-[200px]">
+            <div class="flex flex-col gap-4">
+                <div class="text-center">
+                    <span class="text-white font-bold text-4xl uppercase">name</span>
+                </div>
+                <div class="rounded-[15px] bg-white h-[150px] w-[300px]"></div>
+                <div class="flex justify-center pt-10">
+                    <div class="bg-[#e69700] rounded-[15px] text-center w-[125px] h-[40px] flex justify-center items-center uppercase text-black font-bold text-2xl">visit</div>
+                </div>
+            </div>
+            <div class="flex flex-col gap-4">
+                <div class="text-center">
+                    <span class="text-white font-bold text-4xl uppercase">name</span>
+                </div>
+                <div class="rounded-[15px] bg-white h-[150px] w-[300px]"></div>
+                <div class="flex justify-center pt-10">
+                    <div class="bg-[#e69700] rounded-[15px] text-center w-[125px] h-[40px] flex justify-center items-center uppercase text-black font-bold text-2xl">visit</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
