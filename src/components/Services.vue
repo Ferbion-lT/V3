022abad128fb665ad6d5c9144bfbed1660d34e2f
linusehle <linusfinn.ehle@gmail.com>
2023-11-26 12:31:18 +0100

    feat: changed the padding on the top and on the bottom

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -1,9 +1,12 @@
 <template>
-    <div class="bg-[#191d1e]">
-        <div class="">
-
+    <div class="bg-[#191d1e] pb-[100px]">
+        <div class="pt-[50px] pl-[100px]">
+            <span class="text-[#01a7ee] uppercase font-bold text-xl">services</span>
         </div>
-        <div class="flex flex-row justify-center">
+        <div class="pt-[100px] pb-[75px] flex justify-center">
+            <span class="text-white uppercase font-bold text-4xl">our services</span>
+        </div>
+        <div class="flex flex-row justify-center gap-[250px]">
             <div class="flex flex-col gap-[50px] items-center">
                 <div class="bg-[#272d30] rounded-[15px] text-center h-[400px] w-[300px] flex flex-col items-center justify-center gap-10">
                     <div class="flex justify-center">
@@ -21,6 +24,25 @@
                 </div>
                 <div class="bg-[#e69700] rounded-[15px] h-[50px] w-[125px] text-2xl text-[#272d30] flex items-center justify-center">
                     <span class="font-bold uppercase">create</span>
+                </div>
+            </div>
+            <div class="flex flex-col gap-[50px] items-center">
+                <div class="bg-[#272d30] rounded-[15px] text-center h-[400px] w-[300px] flex flex-col items-center justify-center gap-10">
+                    <div class="flex justify-center">
+                        <img class="h-[150px]" src="../assets/Update.png" />
+                    </div>
+                    <div>
+                        <span class="text-[#01a7ee] font-bold text-4xl">Update</span>
+                    </div>
+                    <div class="text-[#adadad]">
+                        <span>You already have a website but<br>
+                        want to give it a new look?<br>
+                        Then you've come to the right place.
+                        </span>
+                    </div>
+                </div>
+                <div class="bg-[#e69700] rounded-[15px] h-[50px] w-[125px] text-2xl text-[#272d30] flex items-center justify-center">
+                    <span class="font-bold uppercase">update</span>
                 </div>
             </div>
         </div>
