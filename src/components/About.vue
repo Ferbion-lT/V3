c483bb52c231e3952b02089ace9d86445248d35d
linusehle <linusfinn.ehle@gmail.com>
2023-11-26 18:23:07 +0100

    feat: made everything responsive

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,66 +1,74 @@
 <template>
     <div class="bg-[#272d30] pb-[100px]">   
-        <div class="flex flex-row gap-[500px]">
-            <div class="flex flex-col">
-                <div class="pt-[50px] pl-[100px]">
+        <div class="flex flex-col justify-center gap-[500px] sm:flex-row">
+            <div class="flex flex-col justify-center">
+                <div class="pt-[50px] text-center">
                     <span class="text-[#01a7ee] uppercase font-bold text-xl">about us</span>
                 </div>
-                <div class="pt-[50px] pl-[100px]">
+                <div class="pt-[50px] text-center">
                     <span class="text-white font-bold text-4xl">A team with a true<br><span class="text-[#01a7ee]">friendship </span>behind it</span>
                 </div>
-                <div class="pt-[50px] pl-[100px]">
+                <div class="pt-[50px] text-center">
                     <span class="text-white text-xl">We have both been trapped in the depths of<br>
                     web development and web design since 2019.<br>
                     If you want to know more, just contact us! ;)
                 </span>
             </div>
-            <div class="pt-[75px] pl-[200px]">
+            <div class="flex justify-center pt-8">
                 <div class="bg-[#e69700] rounded-[15px] text-center w-[175px] h-[40px] flex justify-center items-center uppercase text-black font-bold text-3xl">contact</div>
             </div>
-            </div>
-            <div class="flex flex-row gap-10 pt-[75px]">
-                <div class="w-[300px] rounded-[15px]">
-                    <div class="bg-white h-[300px] rounded-t-[15px]"></div>
-                    <div class="bg-[#2e2e2e] rounded-b-[15px] h-[50px] text-center flex items-center justify-center">
-                        <span class="text-[#01a7ee] font-bold text-3xl">Linus</span>
-                    </div>
-                    <div class="ml-[10px] mt-4 text-center border-l-4 border-l-[#01a7ee]">
-                        <span class="text-white">Since 2017 Html, Css and Javascript</span><br>
-                        <span class="text-white text-left">Since 2018 Vue and React</span>
+        </div>
+            <div class="flex flex-col gap-10 pt-[75px] sm:flex-row">
+                <div class="flex justify-center">
+                    <div class="w-[300px] rounded-[15px]">
+                        <div class="bg-white h-[300px] rounded-t-[15px]"></div>
+                            <div class="bg-[#2e2e2e] rounded-b-[15px] h-[50px] text-center flex items-center justify-center">
+                            <span class="text-[#01a7ee] font-bold text-3xl">Linus</span>
+                        </div>
+                        <div class="ml-[10px] mt-4 text-center border-l-4 border-l-[#01a7ee]">
+                            <span class="text-white">Since 2017 Html, Css and Javascript</span><br>
+                            <span class="text-white text-left">Since 2018 Vue and React</span>
+                        </div>
                     </div>
                 </div>
-                <div class="w-[300px] rounded-[15px]">
-                    <div class="bg-white h-[300px] rounded-t-[15px]"></div>
-                    <div class="bg-[#2e2e2e] rounded-b-[15px] h-[50px] text-center flex items-center justify-center">
-                        <span class="text-[#01a7ee] font-bold text-3xl">Ben-Luca</span>
-                    </div>
-                    <div class="ml-[10px] mt-4 text-center border-l-4 border-l-[#01a7ee]">
-                        <span class="text-white">Since 2017 Html, Css and Javascript</span><br>
-                        <span class="text-white text-left">Since 2018 Vue and React</span>
+                <div class="flex justify-center">
+                    <div class="w-[300px] rounded-[15px]">
+                        <div class="bg-white h-[300px] rounded-t-[15px]"></div>
+                            <div class="bg-[#2e2e2e] rounded-b-[15px] h-[50px] text-center flex items-center justify-center">
+                            <span class="text-[#01a7ee] font-bold text-3xl">Ben-Luca</span>
+                        </div>
+                        <div class="ml-[10px] mt-4 text-center border-l-4 border-l-[#01a7ee]">
+                            <span class="text-white">Since 2017 Html, Css and Javascript</span><br>
+                            <span class="text-white text-left">Since 2018 Vue and React</span>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="mt-[250px] flex flex-row justify-center gap-[250px]">
-            <div class="bg-white h-[175px] w-[400px] rounded-[15px]"></div>
-            <div class="flex flex-col gap-8">
-                <span class="text-white font-bold text-4xl text-right">Take your business to<br>the <span class="text-[#01a7ee] font-bold">next level</span></span>
-                <span class="text-white text-xl text-right">If you want new customers then you are right here!<br>
+        <div class="mt-[250px] flex flex-col-reverse justify-center gap-[250px] sm:flex-row">
+            <div class="flex justify-center">
+                <div class="bg-white h-[175px] w-[400px] rounded-[15px]"></div>
+            </div>
+            <div class="flex flex-col gap-8 text-center">
+                <span class="text-white font-bold text-4xl text-center sm:text-right">Take your business to<br>the <span class="text-[#01a7ee] font-bold">next level</span></span>
+                <span class="text-white text-xl text-center sm:text-right">If you want new customers then you are right here!<br>
                 We make your website interesting and we make it modern
                 </span>
             </div>
         </div>
-        <div class="mt-[250px] flex flex-row justify-center gap-[250px]">
-            <div class="flex flex-col gap-12">
+        <div class="mt-[250px] flex flex-col justify-center gap-[250px] sm:flex-row">
+            <div class="flex flex-col gap-12 text-center">
                 <span class="text-white font-bold text-4xl">Why You Should <span class="text-[#01a7ee]">Choose Us?</span></span>
-                <div class="flex flex-col border-l-2 border-l-[#01a7ee] pl-4 ml-[75px]">
+                <div class="flex flex-col border-l-2 border-l-[#01a7ee] pl-4 text-center sm:text-left">
                     <span class="text-white text-xl">4 Years of experience on the web</span>
                     <span class="text-white text-xl">A young team of experienced programmers</span>
                     <span class="text-white text-xl">Reliable and friendly</span>
                     <span class="text-white text-xl">Respectful interaction with customers</span>
                 </div>
             </div>
-            <div class="bg-white h-[175px] w-[400px] rounded-[15px]"></div>
+            <div class="flex justify-center">
+                <div class="bg-white h-[175px] w-[400px] rounded-[15px]"></div>
+            </div>
         </div>
     </div>
 </template>
