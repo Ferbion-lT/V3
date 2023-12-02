7a79fee61c8f6591cdc40767417d808f16bdd1d4
Bluc0 <73082882+bluc0@users.noreply.github.com>
2023-12-02 13:24:41 +0100

    Update Projects.vue

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,34 +1,34 @@
 <template>
     <div class="bg-[#272d30]">
         <div class="pt-[50px] flex justify-center sm:pl-[100px] sm:block">
-            <span class="text-[#01a7ee] uppercase font-bold text-xl">projects</span>
+            <span class="text-[#01a7ee] uppercase font-bold text-xl">projekte</span>
         </div>
         <div class="flex flex-col gap-[250px] pt-[250px] sm:flex-row sm:justify-center">
             <div class="flex justify-center flex-col gap-4 lg:hover:scale-125 transition duration-300">
                 <div class="text-center">
-                    <span class="text-white font-bold text-4xl uppercase">name</span>
+                    <span class="text-white font-bold text-4xl uppercase">Karl08</span>
                 </div>
                 <div class="flex justify-center">
                     <div class="rounded-[15px] bg-white h-[150px] w-[300px]"></div>
                 </div>
                 <div class="flex justify-center pt-10">
-                    <a><div class="bg-[#e69700] rounded-[15px] text-center w-[125px] h-[40px] flex justify-center items-center uppercase text-black font-bold text-2xl lg:hover:scale-110 hover:text-white transition duration-150">visit</div></a>
+                    <a><div class="bg-[#e69700] rounded-[15px] text-center w-[125px] h-[40px] flex justify-center items-center uppercase text-black font-bold text-2xl lg:hover:scale-110 hover:text-white transition duration-150">besuchen</div></a>
                 </div>
             </div>
             <div class="flex flex-col justify-center gap-4 lg:hover:scale-125 transition duration-300">
                 <div class="text-center">
-                    <span class="text-white font-bold text-4xl uppercase">name</span>
+                    <span class="text-white font-bold text-4xl uppercase">XYZCRAFT</span>
                 </div>
                 <div class="flex justify-center">
                     <div class="rounded-[15px] bg-white h-[150px] w-[300px]"></div>
                 </div>
                 <div class="flex justify-center pt-10">
-                    <a><div class="bg-[#e69700] rounded-[15px] text-center w-[125px] h-[40px] flex justify-center items-center uppercase text-black font-bold text-2xl lg:hover:scale-110 hover:text-white transition duration-150">visit</div></a>
+                    <a><div class="bg-[#e69700] rounded-[15px] text-center w-[125px] h-[40px] flex justify-center items-center uppercase text-black font-bold text-2xl lg:hover:scale-110 hover:text-white transition duration-150">besuchen</div></a>
                 </div>
             </div>
         </div>
         <div class="pt-[250px] flex justify-center">
-            <span class="text-[#01a7ee] font-bold text-4xl text-center">4 Years <span class="text-white">Of Development<br>Experience</span></span>
+            <span class="text-[#01a7ee] font-bold text-4xl text-center">Über 6 Jahre <span class="text-white">Web <br>Erfahrung</span></span>
         </div>
         <div class="pt-[100px flex flex-col justify-center gap-[250px] pt-[250px] pb-[200px] sm:flex-row">
             <div class="flex flex-col gap-4 lg:hover:scale-125 transition duration-300">
@@ -39,7 +39,7 @@
                     <div class="rounded-[15px] bg-white h-[150px] w-[300px]"></div>
                 </div>
                 <div class="flex justify-center pt-10">
-                    <a><div class="bg-[#e69700] rounded-[15px] text-center w-[125px] h-[40px] flex justify-center items-center uppercase text-black font-bold text-2xl lg:hover:scale-110 hover:text-white transition duration-150">visit</div></a>
+                    <a><div class="bg-[#e69700] rounded-[15px] text-center w-[125px] h-[40px] flex justify-center items-center uppercase text-black font-bold text-2xl lg:hover:scale-110 hover:text-white transition duration-150">besuchen</div></a>
                 </div>
             </div>
             <div class="flex flex-col gap-4 lg:hover:scale-125 transition duration-300">
@@ -50,7 +50,7 @@
                     <div class="rounded-[15px] bg-white h-[150px] w-[300px]"></div>
                 </div>
                 <div class="flex justify-center pt-10">
-                    <a><div class="bg-[#e69700] rounded-[15px] text-center w-[125px] h-[40px] flex justify-center items-center uppercase text-black font-bold text-2xl lg:hover:scale-110 hover:text-white transition duration-150">visit</div></a>
+                    <a><div class="bg-[#e69700] rounded-[15px] text-center w-[125px] h-[40px] flex justify-center items-center uppercase text-black font-bold text-2xl lg:hover:scale-110 hover:text-white transition duration-150">besuchen</div></a>
                 </div>
             </div>
         </div>
