2a399aef085f80ec93ea5680f8444792eaca7730
Bluc0 <73082882+bluc0@users.noreply.github.com>
2023-12-02 10:46:05 +0100

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,19 +5,19 @@
                 <span class="uppercase text-[#515151] font-bold text-5xl">follow us!</span>
             </div>
             <div class="flex flex-row gap-5 pt-[25px]">
-                <div class="h-[50px] w-[50px] rounded-md bg-[#515151] flex justify-center items-center">
+                <div class="h-[50px] w-[50px] rounded-md bg-[#515151] flex justify-center items-center transition duration-150 hover:scale-110 hover:bg-neutral-500">
                     <img class="h-[25px] w-[25px]" src="../assets/Icons/instagram.png" />
                 </div>
-                <div class="h-[50px] w-[50px] rounded-md bg-[#515151] flex justify-center items-center">
+                <div class="h-[50px] w-[50px] rounded-md bg-[#515151] flex justify-center items-center transition duration-150 hover:scale-110 hover:bg-neutral-500">
                     <img class="h-[35px] w-[35px]" src="../assets/Icons/youtube.png" />
                 </div>
-                <div class="h-[50px] w-[50px] rounded-md bg-[#515151] flex justify-center items-center">
+                <div class="h-[50px] w-[50px] rounded-md bg-[#515151] flex justify-center items-center transition duration-150 hover:scale-110 hover:bg-neutral-500">
                     <img class="h-[25px] w-[25px]" src="../assets/Icons/tik-tok.png" />
                 </div>
-                <div class="h-[50px] w-[50px] rounded-md bg-[#515151] flex justify-center items-center">
+                <div class="h-[50px] w-[50px] rounded-md bg-[#515151] flex justify-center items-center transition duration-150 hover:scale-110 hover:bg-neutral-500">
                     <img class="h-[25px] w-[25px]" src="../assets/Icons/twitter.png">
                 </div>
-                <div class="h-[50px] w-[50px] rounded-md bg-[#515151] flex justify-center items-center">
+                <div class="h-[50px] w-[50px] rounded-md bg-[#515151] flex justify-center items-center transition duration-150 hover:scale-110 hover:bg-neutral-500">
                     <img class="h-[25px] w-[25px]" src="../assets/Icons/facebook-app-symbol.png">
                 </div>
             </div>
@@ -25,37 +25,37 @@
     </div>
     <div class="bg-[#191d1e] flex justify-center pt-10 gap-[35%] pb-[75px]">
         <div class="flex flex-col justify-center">
-            <div class="border-l-2 border-[#01a7ee] pl-4">
+            <div class="border-l-2 border-[#01a7ee] pl-4 transition duration-300 hover:scale-110">
                 <ul>
                     <li class="list-none">
-                        <a class="text-[#515151] underline text-lg">Impressum</a>
+                        <a class="text-[#515151] underline text-lg transition duration-150 hover:scale-110 hover:text-[#01a7ee]">Impressum</a>
                     </li>
                     <li class="list-none">
-                        <a class="text-[#515151] underline text-lg">Datenschutz</a>
+                        <a class="text-[#515151] underline text-lg transition duration-150 hover:scale-110 hover:text-[#01a7ee]">Datenschutz</a>
                     </li>
                     <li class="list-none">
-                        <a class="text-[#515151] underline text-lg">Privatsphäre</a>
+                        <a class="text-[#515151] underline text-lg transition duration-150 hover:scale-110 hover:text-[#01a7ee]">Privatsphäre</a>
                     </li>
                     <li class="list-none">
-                        <a class="text-[#515151] underline text-lg">Cookies</a>
+                        <a class="text-[#515151] underline text-lg transition duration-150 hover:scale-110 hover:text-[#01a7ee]">Cookies</a>
                     </li>
                 </ul>
             </div>
         </div>
         <div class="flex flex-col justify-center">
-            <div class="border-r-2 border-[#01a7ee] pr-4">
+            <div class="border-r-2 border-[#01a7ee] pr-4 transition duration-300 hover:scale-110">
                 <ul>
                     <li class="list-none">
-                        <a class="text-[#515151] underline text-xl">Home</a>
+                        <a class="text-[#515151] underline text-xl transition duration-150 hover:scale-110 hover:text-[#01a7ee]">Home</a>
                     </li>
                     <li class="list-none">
-                        <a class="text-[#515151] underline text-lg">About us</a>
+                        <a class="text-[#515151] underline text-lg transition duration-150 hover:scale-110 hover:text-[#01a7ee]">About us</a>
                     </li>
                     <li class="list-none">
-                        <a class="text-[#515151] underline text-lg">Projects</a>
+                        <a class="text-[#515151] underline text-lg transition duration-150 hover:scale-110 hover:text-[#01a7ee]">Projects</a>
                     </li>
                     <li class="list-none">
-                        <a class="text-[#515151] underline text-lg">Contact</a>
+                        <a class="text-[#515151] underline text-lg transition duration-150 hover:scale-110 hover:text-[#01a7ee]">Contact</a>
                     </li>
                 </ul>
             </div>
