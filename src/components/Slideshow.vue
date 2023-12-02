a7b1b1b40a86a570810e27cd22ceb6e1d28289ce
Bluc0 <73082882+bluc0@users.noreply.github.com>
2023-12-02 10:35:27 +0100

    Update Slideshow.vue

--- src/components/Slideshow.vue
+++ src/components/Slideshow.vue
@@ -1,76 +1,76 @@
 <template>
 <div class="flex justify-center">
-	<div class="flex text-white bg-[#0f0f0f] w-full relative overflow-hidden pt-[25px] m-auto h-[200px]">
-		<div class="slide-animation flex">
-			<div class="pl-[25px]">
-				<div class="bg-[#272d30] h-[150px] w-[150px] flex justify-center items-center flex-col rounded-[15px]">
-					<span class="text-[#01a7ee] text-3xl font-bold">+80</span><br>
-					<span class="text-white">Websites</span>
-				</div>
-			</div>
-			<div class="pl-[25px]">
-				<div class="h-[175px] w-[450px] flex items-center">
-					<span class="text-2xl">-- We've Made Already +80 Websites --</span>
-				</div>
-			</div>
-			<div class="pl-[25px]">
-				<div class="bg-[#272d30] h-[150px] w-[150px] flex justify-center items-center flex-col rounded-[15px]">
-					<span class="text-[#01a7ee] text-3xl font-bold">+60</span><br>
-					<span class="text-white">5 Stars Ratings</span>
-				</div>
-			</div>
-			<div class="pl-[25px]">
-				<div class="h-[175px] w-[300px] flex items-center">
-					<span class="text-2xl">-- 5 Stars Ratings, Easy! --</span>
-				</div>
-			</div>
-			<div class="pl-[25px]">
-				<div class="bg-[#272d30] h-[150px] w-[150px] flex justify-center items-center flex-col rounded-[15px]">
-					<span class="text-[#01a7ee] text-3xl font-bold">+580</span><br>
-					<span class="text-white">Hours Of Work</span>
-				</div>
-			</div>			
-			<div class="pl-[25px]">
-				<div class="h-[175px] w-[475px] flex items-center">
-					<span class="text-2xl">-- We Do Our Best For Happy Customers --</span>
-				</div>
-			</div>
-			<div class="pl-[25px]">
-				<div class="bg-[#272d30] h-[150px] w-[150px] flex justify-center items-center flex-col rounded-[15px]">
-					<span class="text-[#01a7ee] text-3xl font-bold">+80</span><br>
-					<span class="text-white">Websites</span>
-				</div>
-			</div>
-			<div class="pl-[25px]">
-				<div class="h-[175px] w-[450px] flex items-center">
-					<span class="text-2xl">-- We've Made Already +80 Websites --</span>
-				</div>
-			</div>
-			<div class="pl-[25px]">
-				<div class="bg-[#272d30] h-[150px] w-[150px] flex justify-center items-center flex-col rounded-[15px]">
-					<span class="text-[#01a7ee] text-3xl font-bold">+60</span><br>
-					<span class="text-white">5 Stars Ratings</span>
-				</div>
-			</div>
-			<div class="pl-[25px]">
-				<div class="h-[175px] w-[300px] flex items-center">
-					<span class="text-2xl">-- 5 Stars Ratings, Easy! --</span>
-				</div>
-			</div>
-			<div class="pl-[25px]">
-				<div class="bg-[#272d30] h-[150px] w-[150px] flex justify-center items-center flex-col rounded-[15px]">
-					<span class="text-[#01a7ee] text-3xl font-bold">+580</span><br>
-					<span class="text-white">Hours Of Work</span>
-				</div>
-			</div>			
-			<div class="pl-[25px]">
-				<div class="h-[175px] w-[475px] flex items-center">
-					<span class="text-2xl">-- We Do Our Best For Happy Customers --</span>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+        <div class="flex text-white bg-[#0f0f0f] w-full relative overflow-hidden pt-[25px] m-auto h-[200px]">
+            <div class="slide-animation flex">
+                <div class="pl-[25px]">
+                    <div class="bg-[#272d30] h-[150px] w-[150px] flex justify-center items-center flex-col rounded-[15px] hover:scale-110 duration-300 transition">
+                        <span class="text-[#01a7ee] text-3xl font-bold">+80</span><br>
+                        <span class="text-white">Websites</span>
+                    </div>
+                </div>
+                <div class="pl-[25px]">
+                    <div class="h-[175px] w-[450px] flex items-center">
+                        <span class="text-2xl">-- We've Made Already +80 Websites --</span>
+                    </div>
+                </div>
+                <div class="pl-[25px]">
+                    <div class="bg-[#272d30] h-[150px] w-[150px] flex justify-center items-center flex-col rounded-[15px] hover:scale-110 duration-300 transition">
+                        <span class="text-[#01a7ee] text-3xl font-bold">+60</span><br>
+                        <span class="text-white">5 Stars Ratings</span>
+                    </div>
+                </div>
+                <div class="pl-[25px]">
+                    <div class="h-[175px] w-[300px] flex items-center">
+                        <span class="text-2xl">-- 5 Stars Ratings, Easy! --</span>
+                    </div>
+                </div>
+                <div class="pl-[25px]">
+                    <div class="bg-[#272d30] h-[150px] w-[150px] flex justify-center items-center flex-col rounded-[15px] hover:scale-110 duration-300 transition">
+                        <span class="text-[#01a7ee] text-3xl font-bold">+580</span><br>
+                        <span class="text-white">Hours Of Work</span>
+                    </div>
+                </div>			
+                <div class="pl-[25px]">
+                    <div class="h-[175px] w-[475px] flex items-center">
+                        <span class="text-2xl">-- We Do Our Best For Happy Customers --</span>
+                    </div>
+                </div>
+                <div class="pl-[25px]">
+                    <div class="bg-[#272d30] h-[150px] w-[150px] flex justify-center items-center flex-col rounded-[15px] hover:scale-110 duration-300 transition">
+                        <span class="text-[#01a7ee] text-3xl font-bold">+80</span><br>
+                        <span class="text-white">Websites</span>
+                    </div>
+                </div>
+                <div class="pl-[25px]">
+                    <div class="h-[175px] w-[450px] flex items-center">
+                        <span class="text-2xl">-- We've Made Already +80 Websites --</span>
+                    </div>
+                </div>
+                <div class="pl-[25px]">
+                    <div class="bg-[#272d30] h-[150px] w-[150px] flex justify-center items-center flex-col rounded-[15px] hover:scale-110 duration-300 transition">
+                        <span class="text-[#01a7ee] text-3xl font-bold">+60</span><br>
+                        <span class="text-white">5 Stars Ratings</span>
+                    </div>
+                </div>
+                <div class="pl-[25px]">
+                    <div class="h-[175px] w-[300px] flex items-center">
+                        <span class="text-2xl">-- 5 Stars Ratings, Easy! --</span>
+                    </div>
+                </div>
+                <div class="pl-[25px]">
+                    <div class="bg-[#272d30] h-[150px] w-[150px] flex justify-center items-center flex-col rounded-[15px] hover:scale-110 duration-300 transition">
+                        <span class="text-[#01a7ee] text-3xl font-bold">+580</span><br>
+                        <span class="text-white">Hours Of Work</span>
+                    </div>
+                </div>			
+                <div class="pl-[25px]">
+                    <div class="h-[175px] w-[475px] flex items-center">
+                        <span class="text-2xl">-- We Do Our Best For Happy Customers --</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 <style>
 
