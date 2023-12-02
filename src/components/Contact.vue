334703d006c2dafe2de372e412819862bc4d6eb9
Bluc0 <73082882+bluc0@users.noreply.github.com>
2023-12-02 13:19:52 +0100

    Update Contact.vue

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -6,7 +6,7 @@
         <div class="flex flex-col-reverse gap-[500px] sm:flex-row">
             <div class="flex flex-col gap-[35px] pt-[150px] sm:pl-[100px]">
                 <div class="flex justify-center transition duration-300 lg:hover:scale-110 hover:text-white">
-                    <input class="bg-[#272d30] rounded-[10px] h-[35px] w-[400px] pl-3" placeholder="Full Name">
+                    <input class="bg-[#272d30] rounded-[10px] h-[35px] w-[400px] pl-3" placeholder="Vor- und Nachname">
                 </div>
                 <div class="flex justify-center transition duration-300 lg:hover:scale-110 hover:text-white">
                     <input class="bg-[#272d30] rounded-[10px] h-[35px] w-[400px] pl-3" placeholder="Email">
@@ -14,31 +14,31 @@
                 <div class="flex justify-center">
                     <label class="text-white" for="type">
                         <select class="bg-[#272d30] w-[400px] h-[35px] rounded-[10px] pl-3" id="type" name="type">
-                            <option value="select">Select Type</option>
-                            <option value="quesions">Questions</option>
-                            <option value="bewerbung">Bewerbung</option>
+                            <option value="select">Betreff</option>
+                            <option value="quesions">Fragen</option>
+                            <option value="bewerbung">Bewerbungen</option>
                             <option value="request">Anfrage</option>
                             <option value="order">Auftrag</option>
                         </select>
                     </label>
                 </div>
                 <div class="flex justify-center transition duration-300 lg:hover:scale-110 hover:text-white">
-                    <input class="bg-[#272d30] rounded-[10px] h-[250px] w-[400px] pl-3" placeholder="Message">
+                    <input class="bg-[#272d30] rounded-[10px] h-[250px] w-[400px] pl-3" placeholder="Nachricht">
                 </div>
                 <div class="flex justify-center">
-                    <a href="#"><div class="bg-[#e69700] rounded-[15px] text-center w-[125px] h-[30px] flex justify-center items-center text-black text-xl transition duration-150 hover:scale-110 hover:text-white">Attach Files</div></a>
+                    <a href="#"><div class="bg-[#e69700] rounded-[15px] text-center w-[125px] h-[30px] flex justify-center items-center text-black text-xl transition duration-150 hover:scale-110 hover:text-white">Datein anhängen</div></a>
                 </div>
                 <div class="flex justify-center pt-10">
-                    <a href="#"><div class="bg-[#e69700] rounded-[15px] text-center w-[175px] h-[40px] flex justify-center items-center uppercase text-black font-bold text-3xl transition duration-300 hover:scale-110 hover:text-white">send</div></a>
+                    <a href="#"><div class="bg-[#e69700] rounded-[15px] text-center w-[175px] h-[40px] flex justify-center items-center uppercase text-black font-bold text-3xl transition duration-300 hover:scale-110 hover:text-white">Senden</div></a>
                 </div>
             </div>
             <div class="mt-[130px]">
                 <div class="flex flex-col gap-[50px] justify-center">
-                    <span class="text-white italic text-4xl text-center">We are happy to help<br>
-                    <span class="text-[#01a7ee]">strengthen your business!</span>
+                    <span class="text-white italic text-4xl text-center">Wir freuen uns sehr, Ihr <br>
+                    <span class="text-[#01a7ee]">Buisness zu stärken</span>
                     </span>
                     <div class="flex">
-                        <span class="text-[#01a7ee] font-bold text-6xl text-center">Get in touch <span class="text-white">with us.</span></span>
+                        <span class="text-[#01a7ee] font-bold text-6xl text-center">Kommen Sie mir uns <span class="text-white">in Kontakt.</span></span>
                     </div>
                 </div>
             </div>
