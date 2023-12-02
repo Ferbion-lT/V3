48f4230de365691a35c868be6782054180a044d3
Bluc0 <73082882+bluc0@users.noreply.github.com>
2023-12-02 13:27:11 +0100

    Update Services.vue

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="bg-[#191d1e] pb-[100px]">
         <div class="pt-[50px] flex justify-center sm:pl-[100px] sm:block">
-            <span class="text-[#01a7ee] uppercase font-bold text-xl">services</span>
+            <span class="text-[#01a7ee] uppercase font-bold text-xl">dienste</span>
         </div>
         <div class="pt-[100px] pb-[75px] flex justify-center">
-            <span class="text-white uppercase font-bold text-4xl">our services</span>
+            <span class="text-white uppercase font-bold text-4xl">Unsere Dienste</span>
         </div>
         <div class="flex flex-col justify-center gap-[250px] sm:flex-row">
             <div class="flex flex-col gap-[50px] items-center lg:hover:scale-110 duration-300 transition">
@@ -13,17 +13,17 @@
                         <img class="h-[125px]" src="../assets/create.png" />
                     </div>
                     <div>
-                        <span class="text-[#01a7ee] font-bold text-4xl">Create</span>
+                        <span class="text-[#01a7ee] font-bold text-4xl">Erstellen</span>
                     </div>
                     <div class="text-[#adadad] hover:text-white transition duration-300">
-                        <span>Are you expanding your business or<br>
-                        do you need a website as an artist?<br>
-                        Then you've come to the right place.
+                        <span>Sie sind gerade dabei, ein Buisness<br>
+                        zu eröffnen oder Sie sind ein Künslter?<br>
+                        Dann sind Sie hier richtig!
                         </span>
                     </div>
                 </div>
                 <a><div class="bg-[#e69700] rounded-[15px] h-[50px] w-[125px] text-2xl text-[#272d30] flex items-center justify-center lg:hover:scale-110 hover:text-white transition duration-150">
-                    <span class="font-bold uppercase">create</span>
+                    <span class="font-bold uppercase">Erstellen</span>
                 </div></a>
             </div>
             <div class="flex flex-col gap-[50px] items-center lg:hover:scale-110 duration-300 transition">
@@ -35,14 +35,14 @@
                         <span class="text-[#01a7ee] font-bold text-4xl">Update</span>
                     </div>
                     <div class="text-[#adadad] hover:text-white transition duration-300">
-                        <span>You already have a website but<br>
-                        want to give it a new look?<br>
-                        Then you've come to the right place.
+                        <span>Sie besitzen bereits eine Website<br>
+                        aber benötigen einen neuen Look?<br>
+                        Dann sind sie hier richtig!
                         </span>
                     </div>
                 </div>
                 <a><div class="bg-[#e69700] rounded-[15px] h-[50px] w-[125px] text-2xl text-[#272d30] flex items-center justify-center lg:hover:scale-110 hover:text-white transition duration-150">
-                    <span class="font-bold uppercase">update</span>
+                    <span class="font-bold uppercase">Update</span>
                 </div></a>
             </div>
         </div>
