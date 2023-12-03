1908293085af6e02b3083ef36847d8b6bcd24b15
Bluc0 <73082882+bluc0@users.noreply.github.com>
2023-12-03 10:57:43 +0100

    Update Projects.vue
    Added ID

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="bg-[#272d30]">
+    <div id="projects" class="bg-[#272d30]">
         <div class="pt-[50px] flex justify-center sm:pl-[100px] sm:block">
             <span class="text-[#01a7ee] uppercase font-bold text-xl">projekte</span>
         </div>
