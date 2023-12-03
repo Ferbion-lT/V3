cfa864e5219711d371e498b6702c1ec5a3588df4
Bluc0 <73082882+bluc0@users.noreply.github.com>
2023-12-03 10:52:05 +0100

    Update Navbar.vue
    Updated Navbar with links

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -19,7 +19,7 @@
               <a class="text-[#00a8f0] uppercase cursor-pointer">start</a>
             </li>
             <li class="list-none">
-              <a class="text-white uppercase cursor-pointer hover:text-[#00a8f0] duration-150 transition">über uns</a>
+              <a href="#about" class="text-white uppercase cursor-pointer hover:text-[#00a8f0] duration-150 transition">über uns</a>
             </li>
             <li class="list-none ">
               <a class="text-white uppercase cursor-pointer hover:text-[#00a8f0] duration-150 transition">dienste</a>
