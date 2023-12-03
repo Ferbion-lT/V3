5175101516d8ac0ade48e4c49cae2403069ce4c2
Bluc0 <73082882+bluc0@users.noreply.github.com>
2023-12-03 11:36:46 +0100

    Update Contact.vue

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,4 +1,4 @@
-<template>
+<template class="scroll-smooth">
     <div id="contact" class="bg-[#191d1e] pb-[250px]">
         <div class="pt-[50px] flex justify-center sm:pl-[100px] sm:block">
             <span class="text-[#01a7ee] uppercase font-bold text-xl">contact</span>
