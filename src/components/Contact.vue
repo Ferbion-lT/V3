cd094b684a94a95cf68eb54f82d28db64c8c845f
Bluc0 <73082882+bluc0@users.noreply.github.com>
2023-12-05 20:35:13 +0100

    Added Pictures

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,47 +1,78 @@
 <template class="scroll-smooth">
-    <div id="contact" class="bg-[#191d1e] pb-[250px]">
+    <body>
+    <div id="about" class="bg-[#272d30] pb-[100px] overflow-hidden">
         <div class="pt-[50px] flex justify-center sm:pl-[100px] sm:block">
-            <span class="text-[#01a7ee] uppercase font-bold text-xl">contact</span>
-        </div>
-        <div class="flex flex-col-reverse gap-[500px] sm:flex-row">
-            <div class="flex flex-col gap-[35px] pt-[150px] sm:pl-[100px]">
-                <div class="flex justify-center transition duration-300 lg:hover:scale-110 hover:text-white">
-                    <input class="bg-[#272d30] rounded-[10px] h-[35px] w-[400px] pl-3" placeholder="Vor- und Nachname">
+            <span class="text-[#01a7ee] uppercase font-bold text-xl">über uns</span>
+        </div>   
+        <div class="transition duration-300 flex flex-col justify-center gap-[500px] sm:flex-row lg:scale-90 lg:hover:scale-95">
+            <div class="flex flex-col justify-center">
+                <div class="pt-[50px] text-left">
+                    <span class="text-white font-bold text-4xl">Ein Team hinter dem<br><span class="text-[#01a7ee]">Freundschaft </span>steht</span>
                 </div>
-                <div class="flex justify-center transition duration-300 lg:hover:scale-110 hover:text-white">
-                    <input class="bg-[#272d30] rounded-[10px] h-[35px] w-[400px] pl-3" placeholder="Email">
-                </div>
-                <div class="flex justify-center">
-                    <label class="text-white" for="type">
-                        <select class="bg-[#272d30] w-[370px] lg:w-[400px] h-[35px] rounded-[10px] pl-3" id="type" name="type">
-                            <option value="select">Betreff</option>
-                            <option value="quesions">Fragen</option>
-                            <option value="bewerbung">Bewerbungen</option>
-                            <option value="request">Anfrage</option>
-                            <option value="order">Auftrag</option>
-                        </select>
-                    </label>
-                </div>
-                <div class="flex justify-center transition duration-300 lg:hover:scale-110 hover:text-white">
-                    <input class="bg-[#272d30] rounded-[10px] h-[250px] w-[400px] pl-3" placeholder="Nachricht">
-                </div>
-                <div class="flex justify-center">
-                    <a href="#"><div class="bg-[#e69700] rounded-[15px] text-center w-[200px] h-[30px] flex justify-center items-center text-black text-xl transition duration-150 hover:scale-110 hover:text-white">Datein anhängen</div></a>
-                </div>
-                <div class="flex justify-center pt-10">
-                    <a href="#"><div class="bg-[#e69700] rounded-[15px] text-center w-[175px] h-[40px] flex justify-center items-center uppercase text-black font-bold text-3xl transition duration-300 hover:scale-110 hover:text-white">Senden</div></a>
-                </div>
+                <div class="pt-[50px] text-left">
+                    <span class="text-white text-xl">Seit 2019 sind wir in den Welten der Webentwicklung und des Webdesign untwegs.
+                    Wenn Sie mehr erfahren möchtest, freuen wir
+                    uns auf Ihre Kontaktaufnahme!
+                </span>
             </div>
-            <div class="mt-[130px]">
-                <div class="flex flex-col gap-[50px] justify-center">
-                    <span class="text-white italic text-4xl text-center">Wir freuen uns sehr, Ihr <br>
-                    <span class="text-[#01a7ee]">Buisness zu stärken</span>
-                    </span>
-                    <div class="flex">
-                        <span class="text-[#01a7ee] font-bold text-6xl text-center">Treten Sie mit uns <br><span class="text-white">in Kontakt.</span></span>
+            <div class="flex justify-center pt-[75px]">
+                <a href="#contact"><div class="transition duration-300 bg-[#e69700] rounded-[15px] text-center w-[175px] h-[40px] flex justify-center items-center uppercase text-black font-bold text-3xl lg:hover:scale-110 hover:text-white">Kontakt</div></a>
+            </div>
+        </div>
+            <div class="flex flex-col gap-10 pt-[75px] sm:flex-row">
+                <div class="flex justify-center">
+                    <div class="w-[300px] rounded-[15px] lg:hover:scale-110 duration-300 transition">
+                        <div class="bg-white"></div>
+                        <img src="../assets/BenLuca.png" class="h-[300px] rounded-t-[15px]">
+                            <div class="bg-[#2e2e2e] rounded-b-[15px] h-[50px] text-center flex items-center justify-center">
+                            <span class="text-[#01a7ee] font-bold text-3xl">Linus</span>
+                        </div>
+                        <div class="ml-[10px] mt-4 text-center border-l-4 border-l-[#01a7ee]">
+                            <span class="text-white">Seit 2017 Html, Css und Javascript</span><br>
+                            <span class="text-white text-left">Seit 2018 Vue und React</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="flex justify-center">
+                    <div class="w-[300px] rounded-[15px] lg:hover:scale-110 duration-300 transition">
+                        <div class="bg-white "></div>
+                            <img src="../assets/BenLuca.png" class="h-[300px] rounded-t-[15px]">
+                            <div class="bg-[#2e2e2e] rounded-b-[15px] h-[50px] text-center flex items-center justify-center">
+                            <span class="text-[#01a7ee] font-bold text-3xl">Ben-Luca</span>
+                        </div>
+                        <div class="ml-[10px] mt-4 text-center border-l-4 border-l-[#01a7ee]">
+                            <span class="text-white">Seit 2020 Html, Css</span><br>
+                            <span class="text-white text-left">Seit 2021 Webdesign / UI Design</span>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+        <div class="transition duration-300 mt-[250px] flex flex-col-reverse justify-center gap-[250px] sm:flex-row lg:hover:scale-110">
+            <div class="flex justify-center">
+                <img src="../assets/stonks.png" class="h-[175px] w-[400px] rounded-[15px]">
+            </div>
+            <div class="flex flex-col gap-8 text-center">
+                <span class="text-white font-bold text-4xl text-center sm:text-right">Bringen Sie ihr Business <br>auf das <span class="text-[#01a7ee] font-bold"> nächste Level</span></span>
+                <span class="text-white text-xl text-center sm:text-right">Sie brauchen neue Kunden oder brauchen einen neuen Look?<br>
+                Wir gestalten Ihre Website modern und interessant!
+                </span>
+            </div>
+        </div>
+        <div class="transition duration-300 mt-[250px] flex flex-col justify-center gap-[250px] sm:flex-row lg:hover:scale-110">
+            <div class="flex flex-col gap-12 text-center">
+                <span class="text-white font-bold text-4xl">Warum Sie uns <span class="text-[#01a7ee]">wählen sollten?</span></span>
+                <div class="flex flex-col gap-4 border-l-2 border-l-[#01a7ee] pl-4 text-center sm:text-left">
+                    <span class="transition duration-300 hover:text-[#01a7ee] text-white text-xl">Mehr als 6 Jahre Erfahrung im Web Development</span>
+                    <span class="transition duration-300 hover:text-[#01a7ee] text-white text-xl">Ein junges und erfahrenes Team</span>
+                    <span class="transition duration-300 hover:text-[#01a7ee] text-white text-xl">Wir sind immer auf dem neusten Stand</span>
+                    <span class="transition duration-300 hover:text-[#01a7ee] text-white text-xl">Zuverlässlich, Respekvoll und Freundlich</span>
+                </div>
+            </div>
+            <div class="flex justify-center">
+                <img src="../assets/handshake.png" class="h-[175px] w-[400px] rounded-[15px]">
+            </div>
+        </div>
+    </div>  
+</body>
 </template>
