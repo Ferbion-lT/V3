fbdfa0926ea493c9ed739828374e81cf0a0eb9e8
linusehle <linusfinn.ehle@gmail.com>
2023-12-16 11:32:13 +0100

    feat: unchanged commit

---
+++ App.vue
@@ -0,0 +1,19 @@
+<script setup>
+ import Navbar from './components/Navbar.vue'
+ import Sidebar from './components/Slideshow.vue'
+ import About from './components/About.vue'
+ import Services from './components/Services.vue'
+ import Projects from './components/Projects.vue'
+ import Contact from './components/Contact.vue'
+ import Footer from './components/Footer.vue'
+</script>
+
+<template>
+    <Navbar />
+    <Sidebar />
+    <About />
+    <Services />
+    <Projects />
+    <Contact />
+    <Footer />
+</template>
