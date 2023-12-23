3d75f266e310f5f6e000e48a9bf7d3e6ce687258
linusehle <linusfinn.ehle@gmail.com>
2023-12-23 21:44:49 +0100

    feat: added router.js

--- router.js
+++ router.js
@@ -1,14 +1,15 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import Impressum from './src/components/Impressum.vue'
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/components/VersionEN',
-      name: 'VersionEN',
-      component: VersionEN
+      path: '/impressum',
+      name: 'Impressum',
+      component: Impressum 
     },
   ],
 });
