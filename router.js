e819232c787f7f8730c4d3c8537d25425e4be3c1
linusehle <linusfinn.ehle@gmail.com>
2023-12-24 13:10:42 +0100

    feat: changed route paths

--- router.js
+++ router.js
@@ -1,15 +1,15 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Impressum from './src/components/Impressum.vue'
+import Home from './src/components/Home.vue'
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/impressum',
-      name: 'Impressum',
-      component: Impressum 
+      path: '/home',
+      name: 'Home',
+      component: Home 
     },
   ],
 });
