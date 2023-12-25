544cd6ab4d52c5fe01bb4593601a43ff40f19454
linusehle <linusfinn.ehle@gmail.com>
2023-12-25 19:56:01 +0100

    feat: added projects section

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -201,6 +201,58 @@
             </div>
         </div>
     </div>
+    <div class="bg-[#000000] text-white overflow-hidden py-14 pb-20">
+        <div class="mx-auto px-4 py-5 sm:px-6 max-w-7xl">
+            <div class="max-w-3xl">
+                <h2 class="font-semibold text-[#01a7ee]">Unsere Arbeit</h2>
+                <p class="mt-2 text-3xl font-bold tracking-tight sm:text-4xl">Portfolio</p>
+                <p class="mt-4 text-zinc-400">
+                    Endecken Sie Beispiele unserer bisherigen Aufträge und gewinnen Sie einen 
+                    Eindruck von der Qualität, welche Sie von uns erwarten können.
+                </p>
+            </div>
+            <div class="mt-10 space-y-16 border-t border-gray-200 pt-10 sm:mt-16">
+                <a class="group mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 lg:flex items-center" href="">
+                    <div>
+                        <div class="text-xl font-bold">M&M Handwerk</div>
+                        <div class="text-zinc-400 w-72">
+                            <div class="mt-2">Website für ein Handwerksunternehmen mit Kontaktformular</div>
+                        </div>
+                    </div>
+                    <div class="lg:flex lg:space-x-6 space-x-0 overscroll-x-auto pl-16">
+                        <div class="hidden lg:block">
+                            <img class="rounded-xl" height="700" width="225" src="../assets/stonks.png" />
+                        </div>
+                        <div class="hidden lg:block">
+                            <img class="rounded-xl" height="700" width="225" src="../assets/stonks.png" />
+                        </div>
+                        <div class="hidden lg:block">
+                            <img class="rounded-xl" height="700" width="225" src="../assets/stonks.png" />
+                        </div>
+                    </div>
+                </a>
+                <a class="group mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 lg:flex items-center" href="">
+                    <div>
+                        <div class="text-xl font-bold">Ben's Burger</div>
+                        <div class="text-zinc-400 w-72">
+                            <div class="mt-2">Webseite für ein Burgerunternehmen</div>
+                        </div>
+                    </div>
+                    <div class="lg:flex lg:space-x-6 space-x-0 overscroll-x-auto pl-16">
+                        <div class="hidden lg:block">
+                            <img class="rounded-xl" height="700" width="225" src="../assets/stonks.png" />
+                        </div>
+                        <div class="hidden lg:block">
+                            <img class="rounded-xl" height="700" width="225" src="../assets/stonks.png" />
+                        </div>
+                        <div class="hidden lg:block">
+                            <img class="rounded-xl" height="700" width="225" src="../assets/stonks.png" />
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
