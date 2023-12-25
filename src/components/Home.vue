7767e7a2c5375666058ffec910a3a0a41b39f490
linusehle <linusfinn.ehle@gmail.com>
2023-12-25 19:54:08 +0100

    feat: added services section

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -138,6 +138,69 @@
             </div>
         </div>
     </div>
+    <div class="bg-[#000000] py-24 sm:py-32 text-white" id="services">
+        <div class="mx-auto max-w-7xl px-6 lg:px-8">
+            <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+                <div>
+                    <h2 class="text-base font-semibold leading-7 text-[#01a7ee]">Web Agentur</h2>
+                    <p class="mt-2 text-3xl font-bold tracking-tight sm:text-4xl">Ferbion Web</p>
+                    <p class="mt-6 text-base leading-7 text-zinc-400">
+                        Wir bieten Ihnen ein umfassendes Angebot, dass alle Aspekte einer modernen, erfolgreichen Website abdeckt.
+                    </p>
+                </div>
+                <dl class="col-span-2 grid grid-cols-1 gap-x-8 gap-y-10 text-base leading-7 sm:grid-cols-2 lg:gap-y-16">
+                    <a>
+                        <div class="relative pl-9 group">
+                            <dt class="font-semibold">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" class="absolute
+                                left-0 top-1 h-5 w-5 text-[#01a7ee]">
+                                    <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0
+                                    01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                                    clip-rule="evenodd"></path>
+                                </svg>  
+                                <h3 class="">Web-Design</h3>
+                            </dt>
+                            <dd class="mt-2 text-zinc-400 hover:no-underline">
+                                Moderne, benutzerfreundliche Design, die auf Ihre Wünsche angepasst sind.
+                            </dd>
+                        </div>
+                    </a>
+                    <a>
+                        <div class="relative pl-9 group">
+                            <dt class="font-semibold">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" class="absolute
+                                left-0 top-1 h-5 w-5 text-[#01a7ee]">
+                                    <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0
+                                    01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                                    clip-rule="evenodd"></path>
+                                </svg>
+                                <h3 class="">Suchmaschienenoptimierung</h3>  
+                            </dt>   
+                            <dd class="mt-2 text-zinc-400 hover:no-underline">
+                                Optimierung Ihrer Webseite für Suchmaschienen, um die Online Sichtbarkeit zu erhöhen.
+                            </dd>
+                        </div>
+                    </a>
+                    <a>
+                        <div class="relative pl-9 group">
+                            <dt class="font-semibold">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" class="absolute
+                                left-0 top-1 h-5 w-5 text-[#01a7ee]">
+                                    <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0
+                                    01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                                    clip-rule="evenodd"></path>
+                                </svg>
+                                <h3 class="">Web-Entwicklung</h3>  
+                            </dt>   
+                            <dd class="mt-2 text-zinc-400 hover:no-underline">
+                                Hochwertige und moderne Entwicklung Ihrer neuen Webseite.
+                            </dd>
+                        </div>
+                    </a>
+                </dl>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
