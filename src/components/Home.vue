2d1fd53a1522aa3ca24b99eed351c0e8ad457105
linusehle <linusfinn.ehle@gmail.com>
2023-12-25 19:48:07 +0100

    feat: added abouts us section

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -72,6 +72,72 @@
             </div>
         </div>
     </div> 
+    <div class="pt-16 sm:pt-32 bg-[#000000] text-white">
+        <div class="mx-auto max-w-7xl px-6 lg:px-8">
+            <div class="mx-auto max-w-3xl lg:text-center">
+                <h2 class="text-base font-semibold leading-7 text-[#01a7ee]">Unsere Leistungen</h2>
+                <p class="mt-2 text-3xl font-bold tracking-tight sm:text-4xl">
+                    Alles was eine professionelle Website braucht.
+                </p>
+                <p class="mt-6 text-lg leading-8 text-zinc-400 text-justify">
+                    Wir sind eine Web-Agentur bestehend aus zwei Personen. die professionelle Websites nicht nur,
+                    designet und SEO maximiert, sondern auch Websites entwickelt. Bei uns haben Sie die Möglichkeit,
+                    eine individuell gestaltete Website zu einem fairen Preis erstellen zu lassen. Unsere Lösungen
+                    sind zudem auf Nachhaltigkeit und Zukunftsfähigkeit ausgerichtet.
+                </p>
+            </div>
+            <div class="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-none">
+                <dl class="grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-3">
+                    <div class="flex flex-col outline outline-1 outline-zinc-800 rounded-lg p-8">
+                        <dt class="flex items-center gap-x-3 text-base font-semibold leading-7">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" class="h-5 w-5 flex-none text-[#01a7ee]">
+                                <path d="M2 4.25A2.25 2.25 0 014.25 2h2.5A2.25 2.25 0 019 4.25v2.5A2.25 2.25 0 016.75 9h-2.5A2.25 2.25 0 012 6.75v-2.5zM2 13.25A2.25 2.25 0 014.25
+                                11h2.5A2.25 2.25 0 019 13.25v2.5A2.25 2.25 0 016.75 18h-2.5A2.25 2.25 0 012 15.75v-2.5zM11 4.25A2.25 2.25 0 0113.25 2h2.5A2.25 2.25 0 0118 4.25v2.5A2.25
+                                2.25 0 0115.75 9h-2.5A2.25 2.25 0 0111 6.75v-2.5zM15.25 11.75a.75.75 0 00-1.5 0v2h-2a.75.75 0 000 1.5h2v2a.75.75 0 001.5 0v-2h2a.75.75 0 000-1.5h-2v-2z">
+                                </path>
+                            </svg> 
+                            Web-Design
+                        </dt>
+                        <dd class="mt-4 flex flex-auto flex-col text-base leading-7">
+                            <p class="flex-auto">
+                                Hochwertiges und benutzerfreundliches Design für optimales Nutzererlebnis.
+                            </p>
+                        </dd>
+                    </div>
+                    <div class="flex flex-col outline outline-1 outline-zinc-800 rounded-lg p-8">
+                        <dt class="flex items-center gap-x-3 text-base font-semibold leading-7">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" class="h-5 w-5 flex-none text-[#01a7ee]">
+                                <path fill-rule="evenodd" d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7
+                                 7 0 012 9z" clip-rule="evenodd">
+                                </path>
+                            </svg> 
+                            Suchmaschienenoptimierung       
+                        </dt>
+                        <dd class="mt-4 flex flex-auto flex-col text-base leading-7">
+                            <p class="flex-auto">
+                                SEO gemäß Google-Standarts für langfristige Erfolge.
+                            </p>
+                        </dd>
+                    </div>
+                    <div class="flex flex-col outline outline-1 outline-zinc-800 rounded-lg p-8">
+                        <dt class="flex items-center gap-x-3 text-base font-semibold leading-7">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="h-5 w-5 text-[#01a7ee]" viewBox="0 0 16 16">
+                                <path d="M10.478 1.647a.5.5 0 1 0-.956-.294l-4 13a.5.5 0 0 0 .956.294l4-13zM4.854 4.146a.5.5 0 0 1 0 .708L1.707 8l3.147 
+                                3.146a.5.5 0 0 1-.708.708l-3.5-3.5a.5.5 0 0 1 0-.708l3.5-3.5a.5.5 0 0 1 .708 0zm6.292 0a.5.5 0 0 0 0 .708L14.293 8l-3.147
+                                3.146a.5.5 0 0 0 .708.708l3.5-3.5a.5.5 0 0 0 0-.708l-3.5-3.5a.5.5 0 0 0-.708 0z"/>
+                            </svg>
+                            Web-Entwicklung    
+                        </dt>
+                        <dd class="mt-4 flex flex-auto flex-col text-base leading-7">
+                            <p class="flex-auto">
+                                Hochwertige und moderne Entwicklung Ihrer neuen Webseite.
+                            </p>
+                        </dd>
+                    </div>
+                </dl>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
