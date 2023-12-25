dc741d5fe6aec58471c62d35b96f2bc38beed706
linusehle <linusfinn.ehle@gmail.com>
2023-12-25 20:59:31 +0100

    feat: added impressum for the website

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
 import { RouterView } from 'vue-router';
 import Home from './components/Home.vue'
+import Impressum from './components/Impressum.vue'
 </script>
 
 <template>
-    <Home />
+    <Impressum />
 </template>
 
--- src/components/Impressum.vue
+++ src/components/Impressum.vue
@@ -1,5 +1,59 @@
 <template>
-<div>
-    Hi kurzer Test
-</div>    
+    <body class="bg-[#000000] text-white">
+        <div class="bg-[#000000] text-white container mx-auto p-8">
+            <h1 class="text-4xl font-semibold mb-4">Impressum</h1>
+            <div class="text-lg">
+                <h2 class="font-semibold mb-2">Angaben gem. § 5 TMG:</h2>
+                <p>Linus Ehle</p>
+                <p>Olgastaße 88/1, 73779 Deizisau, Baden Württenberg, Deutschland</p>
+                <p class="mt-4">Ben-Luca Gering</p>
+                <p>Am Kappellenhof 26, 53783 Eitorf, Nordrhein-Westfalen, Deutschland</p>
+                <h2 class="font-semibold mt-4 mb-2">Kontaktaufnahme Linus Ehle:</h2>
+                <p>Telefon: 01781409590</p>
+                <p>Fax: - </p>
+                <p>E-Mail: linusehle@ferbion.com</p>
+                <h2 class="font-semibold mt-4 mb-2">Kontaktaufnahme Ben-Luca Gering:</h2>
+                <p>Telefon: 015253144275</p>
+                <p>Fax: - </p>
+                <p>E-Mail: benlucagering@ferbion.com</p>
+                <h2 class="font-semibold mt-4 mb-2">Umsatzsteuer-ID</h2>
+                <p>Umsatzsteuer-Identifikationsnummer gem. § 27 a Umsatzsteuergesetz:</p>
+                <p>- (nicht vorhanden)</p>
+                <h2 class="font-semibold mt-4 mb-2">Haftungsausschluss - Disclaimer:</h2>
+                <h3 class="font-semibold mt-2 mb-1">Haftung für Inhalte</h3>
+                <p>Alle Inhalte unseres Internetauftritts wurden mit größter Sorgfalt und nach bestem Gewissen erstellt. ...</p>
+                <p>Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir jedoch keine Gewähr übernehmen. Als
+                Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen
+                verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder
+                gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige
+                Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen
+                Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der
+                Kenntniserlangung einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von den o.g. Rechtsverletzungen werden
+                wir diese Inhalte unverzüglich entfernen. Haftungsbeschränkung für externe Links Unsere Webseite enthält Links auf
+                externe Webseiten Dritter. Auf die Inhalte dieser direkt oder indirekt verlinkten Webseiten haben wir keinen Einfluss.
+                Daher können wir für die „externen Links“ auch keine Gewähr auf Richtigkeit der Inhalte übernehmen. Für die Inhalte
+                der externen Links sind die jeweilige Anbieter oder Betreiber (Urheber) der Seiten verantwortlich. Die externen Links
+                wurden zum Zeitpunkt der Linksetzung auf eventuelle Rechtsverstöße überprüft und waren im Zeitpunkt der Linksetzung frei
+                von rechtswidrigen Inhalten. Eine ständige inhaltliche Überprüfung der externen Links ist ohne konkrete Anhaltspunkte
+                einer Rechtsverletzung nicht möglich. Bei direkten oder indirekten Verlinkungen auf die Webseiten Dritter, die außerhalb
+                unseres Verantwortungsbereichs liegen, würde eine Haftungsverpflichtung ausschließlich in dem Fall nur bestehen, wenn
+                wir von den Inhalten Kenntnis erlangen und es uns technisch möglich und zumutbar wäre, die Nutzung im Falle rechtswidriger
+                Inhalte zu verhindern. Diese Haftungsausschlusserklärung gilt auch innerhalb des eigenen Internetauftrittes
+                „Name Ihrer Domain“ gesetzten Links und Verweise von Fragestellern, Blogeinträgern, Gästen des Diskussionsforums.
+                Für illegale, fehlerhafte oder unvollständige Inhalte und insbesondere für Schäden, die aus der Nutzung oder Nichtnutzung
+                solcherart dargestellten Informationen entstehen, haftet allein der Diensteanbieter der Seite, auf welche verwiesen wurde,
+                nicht derjenige, der über Links auf die jeweilige Veröffentlichung lediglich verweist. Werden uns Rechtsverletzungen
+                bekannt, werden die externen Links durch uns unverzüglich entfernt. Urheberrecht Die auf unserer Webseite veröffentlichen
+                Inhalte und Werke unterliegen dem deutschen Urheberrecht (http://www.gesetze-im-internet.de/bundesrecht/urhg/gesamt.pdf) .
+                Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung des geistigen Eigentums in ideeller und materieller
+                Sicht des Urhebers außerhalb der Grenzen des Urheberrechtes bedürfen der vorherigen schriftlichen Zustimmung des jeweiligen
+                Urhebers i.S.d. Urhebergesetzes (http://www.gesetze-im-internet.de/bundesrecht/urhg/gesamt.pdf ). Downloads und Kopien dieser
+                Seite sind nur für den privaten und nicht kommerziellen Gebrauch erlaubt. Sind die Inhalte auf unserer Webseite nicht von uns
+                erstellt wurden, sind die Urheberrechte Dritter zu beachten. Die Inhalte Dritter werden als solche kenntlich gemacht. Sollten
+                Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden
+                von Rechtsverletzungen werden wir derartige Inhalte unverzüglich entfernen. Dieses Impressum wurde freundlicherweise von 
+                jurarat.de zur Verfügung gestellt.</p>
+            </div>
+        </div>
+    </body>
 </template>
