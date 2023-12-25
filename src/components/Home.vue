e87fea25c627dd7d043d20ab72d68a97fb9b341a
linusehle <linusfinn.ehle@gmail.com>
2023-12-25 19:57:23 +0100

    feat: added footer section

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -253,6 +253,84 @@
             </div>
         </div>
     </div>
+    <div class="bg-zinc-950 border-t-4 border-zinc-900 text-white">
+        <div class="mx-auto max-w-7xl px-6 py-16 sm:py-24 lg:px-8 lg:py-32">
+            <div class="xl: grid xl:grid-cols-3 xl:gap-8">
+                <img class=" h-28" src="../assets/logo_transparent.png"/>
+                <div class="mt-16 grid grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
+                    <div class="md:grid md:grid-cols-2 md:gap-8">
+                        <div>
+                            <h3 class="text-sm font-semibold leading-6">Webseite</h3>
+                            <ul class="mt-6 space-y-4" role="list">
+                                <li>
+                                    <a class="text-sm leading-6 text-zinc-400" href="">Start</a>
+                                </li>
+                                <li>
+                                    <a class="text-sm leading-6 text-zinc-400" href="">Portfolio</a>
+                                </li>
+                                <li>
+                                    <a class="text-sm leading-6 text-zinc-400" href="">Leistungen</a>
+                                </li>
+                                <li>
+                                    <a class="text-sm leading-6 text-zinc-400" href="">Kontakt</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="mt-10 md:mt-0">
+                            <h3 class="text-sm font-semibold leading-6">Support</h3>
+                            <ul class="mt-6 space-y-4" role="list">
+                                <li>
+                                    <a class="text-sm leading-6 text-zinc-400" href="">Zahlungsmethoden</a>
+                                </li>          
+                                <li>
+                                    <a class="text-sm leading-6 text-zinc-400" href="">Sitemap</a>
+                                </li>
+                                <li>
+                                    <a class="text-sm leading-6 text-zinc-400" href="">Status</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="md:grid md:grid-cols-2 md:gap-8">
+                        <div>
+                            <h3 class="text-sm font-semibold leading-6">Soziale Medien</h3>        
+                            <ul class="mt-6 space-y-4" role="list">
+                                <li>
+                                    <a class="text-sm leading-6 text-zinc-400" href="https://www.instagram.com/ferbion_web/">Instagram</a>
+                                </li>
+                                <li>
+                                    <a class="text-sm leading-6 text-zinc-400" href="https://www.youtube.com/channel/UC-UchX30BaknW8U25JiSgDA">Youtube</a>
+                                </li>
+                                <li>
+                                    <a class="text-sm leading-6 text-zinc-400" href="https://www.tiktok.com/@ferbionweb">Tiktok</a>
+                                </li>
+                                <li>
+                                    <a class="text-sm leading-6 text-zinc-400" href="https://twitter.com/FerbionWeb">X / Twitter</a>
+                                </li>
+                                <li>
+                                    <a class="text-sm leading-6 text-zinc-400" href="">Facebook</a>
+                                </li>
+                            </ul>       
+                        </div>
+                        <div class="mt-10 md:mt-0">
+                            <h3 class="text-sm font-semibold leading-6">Rechtliches</h3>
+                            <ul class="mt-6 space-y-4" role="list">
+                                <li>
+                                    <a class="text-sm leading-6 text-zinc-400" href="">Impressum</a>
+                                </li>
+                                <li>
+                                    <a class="text-sm leading-6 text-zinc-400" href="">Datenschutz</a>
+                                </li>
+                                <li>
+                                    <a class="text-sm leading-6 text-zinc-400" href="">AGB</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
