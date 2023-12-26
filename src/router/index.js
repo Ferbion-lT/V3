223808e7816389d4e45ed64989d53e59dec68c75
linusehle <linusfinn.ehle@gmail.com>
2023-12-26 18:23:16 +0100

    feat: added contact route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../components/Home.vue'
 import Impressum from '../components/Impressum.vue'
+import Contact from '../components/Contact.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/impressum',
       name: 'impressum',
       component: Impressum
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: Contact
     }
   ]
 })
