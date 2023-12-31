45fa469989f21985e53cd9b731a0c07f2230c579
linusehle <linusfinn.ehle@gmail.com>
2023-12-31 14:08:01 +0100

    feat: improved responsivness of the navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,52 +1,41 @@
-<template class="scroll-smooth">
-  <div id="start" class="bg-[#000000] text-white">
-        <header class=" bg-zinc-950 border-b-4 border-zinc-900 sm:flex sm:justify-center pt-10">
-        <div class="flex pl-[7.5%] px-4 py-3 sm:p-0">
-          <div class="sm:hidden">
-            <button @click="isOpen = !isOpen" type="button" class="block text-white">
-              <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
-                <path v-if="isOpen" fill-rule="evenodd" d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"/>
-                <path v-if="!isOpen" fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"/>
-              </svg>
-            </button>
+<template>
+  <header class="bg-zinc-950 text-white md:flex md:justify-center md:items-center">
+    <div class="flex items-center justify-between px-4 pb-4 py-3 sm:p-0">
+      <div class="pt-4 pl-4 pb-4 md:hidden">
+        <button @click="isOpen = !isOpen" type="button" class="block text-white focus:text-white focus:outline-none">
+          <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
+            <path v-if="isOpen" fill-rule="evenodd" d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"/>
+            <path v-if="!isOpen" fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"/>
+          </svg>
+        </button>
+      </div>
+    </div>
+    <nav :class="isOpen ? 'flex' : 'hidden'" class="md:flex">
+      <ul class="flex text-center w-full flex-col space-y-4 md:flex-row md:space-y-0 md:space-x-8 md:justify-center md:items-center max-[884px]:pt-8 pb-4">
+          <li>
+            <router-link class="text-lg font-semibold leading-6 hover:text-[#00a8f0]" :class="{'text-[#00a8f0]': $route.path === '/'}" to="/">Home</router-link>
+          </li>
+          <li>
+            <span class="text-lg font-semibold leading-6 hover:text-[#00a8f0] cursor-pointer transition" v-on:click="about_scroll">Über uns</span>
+          </li>
+          <li>
+            <span class="text-lg font-semibold left-6 hover:text-[#00a8f0] cursor-pointer transition" v-on:click="services_scroll">Leistungen</span>
+          </li>
+          <div class="hidden min-[884px]:block">
+            <img class="h-[100px]" src="../assets/logo_transparent.png">
           </div>
-        </div>
-        <nav :class="isOpen ? 'flex' : 'hidden'" class="flex-col px-2 pt-2 pb-4 sm:flex justify-center sm:p-0">
-          <div class="flex flex-col pl-[7.5%] gap-10 md:flex-row sm:pl-0 sm:justify-center">
-            <div class="">
-            <ul class="flex flex-col sm:flex-row gap-10 md:pr-[100px]">
-            <li class="list-none">
-              <router-link class="text-lg font-semibold leading-6 hover:text-[#00a8f0]" :class="{'text-[#00a8f0]': $route.path === '/'}" to="/">Home</router-link>
-            </li>
-            <li class="list-none">    
-              <span class="text-lg font-semibold leading-6 hover:text-[#00a8f0] cursor-pointer transition" v-on:click="about_scroll">Über uns</span>
-            </li>
-            <li class="list-none ">
-              <span class="text-lg font-semibold left-6 hover:text-[#00a8f0] cursor-pointer transition" v-on:click="services_scroll">Leistungen</span>
-            </li>
-          </ul>
-        </div>
-        <div class="hidden md:block md:justify-center relative mt-[-3%]">
-          <img class="h-[90px] w-[190px] lg:hover:scale-110 duration-300 transition" src="../assets/logo_transparent.png"> 
-        </div>
-        <div class="">
-          <ul class="flex flex-col sm:flex-row gap-10 md:pl-[100px]">
-            <li class="list-none">
-              <span class="text-lg font-semibold leading-6 hover:text-[#00a8f0] cursor-pointer transition" v-on:click="portfolio_scroll">Portfolio</span>
-            </li>
-            <li class="list-none">
-              <router-link class="text-lg font-semibold leading-6 hover:text-[#00a8f0]" :class="{'text-[#00a8f0]': $route.path === '/contact'}" to="/contact">Kontakt</router-link>
-            </li>
-            <li class="list-none">
-              <router-link class="text-lg font-semibold leading-6 hover:text-[#00a8f0]" :class="{'text-[#00a8f0]': $route.path === '/impressum'}" to="/impressum">Impressum</router-link>
-            </li>
-          </ul>
-          </div>
-          </div>
-        </nav>
-      </header>
-  </div>
-    
+          <li>
+            <span class="text-lg font-semibold leading-6 hover:text-[#00a8f0] cursor-pointer transition" v-on:click="portfolio_scroll">Portfolio</span>
+          </li>
+          <li>
+            <router-link class="text-lg font-semibold leading-6 hover:text-[#00a8f0]" :class="{'text-[#00a8f0]': $route.path === '/contact'}" to="/contact">Kontakt</router-link>
+          </li>
+          <li>
+            <router-link class="text-lg font-semibold leading-6 hover:text-[#00a8f0]" :class="{'text-[#00a8f0]': $route.path === '/impressum'}" to="/impressum">Impressum</router-link>
+          </li>
+        </ul>
+    </nav>
+  </header>
 </template>
 
 <script>
@@ -56,21 +45,5 @@ export default {
       isOpen: false,
     }
   },
-}
-
-
-</script>
-
-<script setup>
-function about_scroll (event) {
-  document.getElementById("leistungen").scrollIntoView({behavior: 'smooth'})
-}
-
-function services_scroll (event) {
-  document.getElementById("services").scrollIntoView({behavior: 'smooth'})
-}
-
-function portfolio_scroll (event) {
-  document.getElementById("portfolio").scrollIntoView({behavior: 'smooth'})
 }
 </script>
