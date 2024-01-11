8bce66df4f6d7c64e1fbcae4a6dd5d9414ee9b4f
linusehle <linusfinn.ehle@gmail.com>
2024-01-11 13:37:23 +0100

    feat: Inital commit

---
+++ src/components/En.vue
@@ -0,0 +1,231 @@
+<template>
+    <div id="start" class="bg-[#000000] text-white">
+        <div class="relative">
+            <div class="mx-auto max-w-7xl lg:grid lg:grid-cols-12 lg:gap-x-8 lg:px-8">
+                <div class="px-6 pb-24 pt-10 sm:pb-32 lg:col-span-7 lg:px-0 lg:pb-56 lg:pt-48 xl:col-span-6">
+                    <div class="mx-auto max-w-2xl lg:mx-0">
+                        <div class="mt-24 text-4xl font-bold tracking-tight sm:mt-10 sm:text-6xl">
+                            <h1>We create</h1>
+                            <h1>professional Websites</h1>
+                        </div>
+                        <div class="mt-10 flex items-center gap-x-6">
+                            <a class="bg-[#00a8f0] rounded-full p-2 text-lg px-3 text-white hover:scale-105 transition" href="">
+                                Textidee?
+                            </a>
+                            <a class="text-lg font-semibold leading-6 hover:text-[#00a8f0] transition" href="">
+                                More information
+                                <span aria-hidden="true">→</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="relative lg:col-span-5 lg:-mr-8 xl:absolute xl:inset-0 xl:left-1/2 xl:mr-0">
+                    <img class="w-full aspect-[3/2] object-cover lg:absolute lg:inset-0 lg:aspect-auto lg:h-full"
+                    height="2180" style="color: transparent;"
+                    src="../assets/Mockup.png" />
+                </div>
+            </div>
+        </div>
+    </div> 
+    <div class="pt-16 sm:pt-32 bg-[#000000] text-white" id="leistungen">
+        <div class="mx-auto max-w-7xl px-6 lg:px-8">
+            <div class="mx-auto max-w-3xl lg:text-center">
+                <h2 class="text-base font-semibold leading-7 text-[#01a7ee]">Our services</h2>
+                <p class="mt-2 text-3xl font-bold tracking-tight sm:text-4xl">
+                    Alles was eine professionelle Website braucht.
+                </p>
+                <p class="mt-6 text-lg leading-8 text-zinc-400 text-justify">
+                    Wir sind eine Web-Agentur bestehend aus zwei Personen. die professionelle Websites nicht nur,
+                    designet und SEO maximiert, sondern auch Websites entwickelt. Bei uns haben Sie die Möglichkeit,
+                    eine individuell gestaltete Website zu einem fairen Preis erstellen zu lassen. Unsere Lösungen
+                    sind zudem auf Nachhaltigkeit und Zukunftsfähigkeit ausgerichtet.
+                </p>
+            </div>
+            <div class="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-none">
+                <dl class="grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-3">
+                    <div class="flex flex-col outline outline-1 outline-zinc-800 rounded-lg p-8">
+                        <dt class="flex items-center gap-x-3 text-base font-semibold leading-7">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" class="h-5 w-5 flex-none text-[#01a7ee]">
+                                <path d="M2 4.25A2.25 2.25 0 014.25 2h2.5A2.25 2.25 0 019 4.25v2.5A2.25 2.25 0 016.75 9h-2.5A2.25 2.25 0 012 6.75v-2.5zM2 13.25A2.25 2.25 0 014.25
+                                11h2.5A2.25 2.25 0 019 13.25v2.5A2.25 2.25 0 016.75 18h-2.5A2.25 2.25 0 012 15.75v-2.5zM11 4.25A2.25 2.25 0 0113.25 2h2.5A2.25 2.25 0 0118 4.25v2.5A2.25
+                                2.25 0 0115.75 9h-2.5A2.25 2.25 0 0111 6.75v-2.5zM15.25 11.75a.75.75 0 00-1.5 0v2h-2a.75.75 0 000 1.5h2v2a.75.75 0 001.5 0v-2h2a.75.75 0 000-1.5h-2v-2z">
+                                </path>
+                            </svg> 
+                            Web-Design
+                        </dt>
+                        <dd class="mt-4 flex flex-auto flex-col text-base leading-7">
+                            <p class="flex-auto">
+                                Hochwertiges und benutzerfreundliches Design für optimales Nutzererlebnis.
+                            </p>
+                        </dd>
+                    </div>
+                    <div class="flex flex-col outline outline-1 outline-zinc-800 rounded-lg p-8">
+                        <dt class="flex items-center gap-x-3 text-base font-semibold leading-7">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" class="h-5 w-5 flex-none text-[#01a7ee]">
+                                <path fill-rule="evenodd" d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7
+                                 7 0 012 9z" clip-rule="evenodd">
+                                </path>
+                            </svg> 
+                            Suchmaschienenoptimierung       
+                        </dt>
+                        <dd class="mt-4 flex flex-auto flex-col text-base leading-7">
+                            <p class="flex-auto">
+                                SEO gemäß Google-Standarts für langfristige Erfolge.
+                            </p>
+                        </dd>
+                    </div>
+                    <div class="flex flex-col outline outline-1 outline-zinc-800 rounded-lg p-8">
+                        <dt class="flex items-center gap-x-3 text-base font-semibold leading-7">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="h-5 w-5 text-[#01a7ee]" viewBox="0 0 16 16">
+                                <path d="M10.478 1.647a.5.5 0 1 0-.956-.294l-4 13a.5.5 0 0 0 .956.294l4-13zM4.854 4.146a.5.5 0 0 1 0 .708L1.707 8l3.147 
+                                3.146a.5.5 0 0 1-.708.708l-3.5-3.5a.5.5 0 0 1 0-.708l3.5-3.5a.5.5 0 0 1 .708 0zm6.292 0a.5.5 0 0 0 0 .708L14.293 8l-3.147
+                                3.146a.5.5 0 0 0 .708.708l3.5-3.5a.5.5 0 0 0 0-.708l-3.5-3.5a.5.5 0 0 0-.708 0z"/>
+                            </svg>
+                            Web-Entwicklung    
+                        </dt>
+                        <dd class="mt-4 flex flex-auto flex-col text-base leading-7">
+                            <p class="flex-auto">
+                                Hochwertige und moderne Entwicklung Ihrer neuen Webseite.
+                            </p>
+                        </dd>
+                    </div>
+                </dl>
+            </div>
+        </div>
+    </div>
+    <div class="bg-[#000000] py-24 sm:py-32 text-white" id="services">
+        <div class="mx-auto max-w-7xl px-6 lg:px-8">
+            <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+                <div>
+                    <h2 class="text-base font-semibold leading-7 text-[#01a7ee]">Web Agentur</h2>
+                    <p class="mt-2 text-3xl font-bold tracking-tight sm:text-4xl">Ferbion Web</p>
+                    <p class="mt-6 text-base leading-7 text-zinc-400">
+                        Wir bieten Ihnen ein umfassendes Angebot, dass alle Aspekte einer modernen, erfolgreichen Website abdeckt.
+                    </p>
+                </div>
+                <dl class="col-span-2 grid grid-cols-1 gap-x-8 gap-y-10 text-base leading-7 sm:grid-cols-2 lg:gap-y-16">
+                    <a>
+                        <div class="relative pl-9 group">
+                            <dt class="font-semibold">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" class="absolute
+                                left-0 top-1 h-5 w-5 text-[#01a7ee]">
+                                    <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0
+                                    01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                                    clip-rule="evenodd"></path>
+                                </svg>  
+                                <h3 class="">Web-Design</h3>
+                            </dt>
+                            <dd class="mt-2 text-zinc-400 hover:no-underline">
+                                Moderne, benutzerfreundliche Design, die auf Ihre Wünsche angepasst sind.
+                            </dd>
+                        </div>
+                    </a>
+                    <a>
+                        <div class="relative pl-9 group">
+                            <dt class="font-semibold">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" class="absolute
+                                left-0 top-1 h-5 w-5 text-[#01a7ee]">
+                                    <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0
+                                    01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                                    clip-rule="evenodd"></path>
+                                </svg>
+                                <h3 class="">Suchmaschienenoptimierung</h3>  
+                            </dt>   
+                            <dd class="mt-2 text-zinc-400 hover:no-underline">
+                                Optimierung Ihrer Website für Suchmaschienen, um die Online Sichtbarkeit zu erhöhen.
+                            </dd>
+                        </div>
+                    </a>
+                    <a>
+                        <div class="relative pl-9 group">
+                            <dt class="font-semibold">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" class="absolute
+                                left-0 top-1 h-5 w-5 text-[#01a7ee]">
+                                    <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0
+                                    01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                                    clip-rule="evenodd"></path>
+                                </svg>
+                                <h3 class="">Web-Development</h3>  
+                            </dt>   
+                            <dd class="mt-2 text-zinc-400 hover:no-underline">
+                                Hochwertige und moderne Entwicklung Ihrer neuen Website.
+                            </dd>
+                        </div>
+                    </a>
+                    <a>
+                        <div class="relative pl-9 group">
+                            <dt class="font-semibold">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" class="absolute
+                                left-0 top-1 h-5 w-5 text-[#01a7ee]">
+                                    <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0
+                                    01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                                    clip-rule="evenodd"></path>
+                                </svg>
+                                <h3 class="">Hosting Unterstützung</h3>  
+                            </dt>   
+                            <dd class="mt-2 text-zinc-400 hover:no-underline">
+                                Untertützung beim Uploud der Website
+                            </dd>
+                        </div>
+                    </a>
+                </dl>
+            </div>
+        </div>
+    </div>
+    <div class="bg-[#000000] text-white overflow-hidden py-14 pb-20" id="portfolio">
+        <div class="mx-auto px-4 py-5 sm:px-6 max-w-7xl">
+            <div class="max-w-3xl">
+                <h2 class="font-semibold text-[#01a7ee]">Unsere Arbeit</h2>
+                <p class="mt-2 text-3xl font-bold tracking-tight sm:text-4xl">Portfolio</p>
+                <p class="mt-4 text-zinc-400">
+                    Endecken Sie Beispiele unserer bisherigen Aufträge und gewinnen Sie einen 
+                    Eindruck von der Qualität, welche Sie von uns erwarten können.
+                </p>
+            </div>
+            <div class="mt-10 space-y-16 border-t border-gray-200 pt-10 sm:mt-16">
+                <a class="group mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 lg:flex items-center" href="">
+                    <div>
+                        <div class="text-xl font-bold">M&M Handwerk</div>
+                        <div class="text-zinc-400 w-72">
+                            <div class="mt-2">Website für ein Handwerksunternehmen mit Kontaktformular</div>
+                        </div>
+                    </div>
+                    <div class="lg:flex lg:space-x-6 space-x-0 overscroll-x-auto pl-16">
+                        <div class="hidden lg:block">
+                            <img class="rounded-xl" height="700" width="225" src="../assets/stonks.png" />
+                        </div>
+                        <div class="hidden lg:block">
+                            <img class="rounded-xl" height="700" width="225" src="../assets/stonks.png" />
+                        </div>
+                        <div class="hidden lg:block">
+                            <img class="rounded-xl" height="700" width="225" src="../assets/stonks.png" />
+                        </div>
+                    </div>
+                </a>
+                <a class="group mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 lg:flex items-center" href="">
+                    <div>
+                        <div class="text-xl font-bold">Ben's Burger</div>
+                        <div class="text-zinc-400 w-72">
+                            <div class="mt-2">Webseite für ein Burgerunternehmen</div>
+                        </div>
+                    </div>
+                    <div class="lg:flex lg:space-x-6 space-x-0 overscroll-x-auto pl-16">
+                        <div class="hidden lg:block">
+                            <img class="rounded-xl" height="700" width="225" src="../assets/stonks.png" />
+                        </div>
+                        <div class="hidden lg:block">
+                            <img class="rounded-xl" height="700" width="225" src="../assets/stonks.png" />
+                        </div>
+                        <div class="hidden lg:block">
+                            <img class="rounded-xl" height="700" width="225" src="../assets/stonks.png" />
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
